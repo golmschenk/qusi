38d3c9844426378db4ce6f1babd8cc19d12591ab
Greg Olmschenk <golmschenk@gmail.com>
2019-05-18 15:48:30 -0400

    Add functional pytest configuration

---
+++ conftest.py
@@ -0,0 +1,26 @@
+"""Configuration for the pytest tests."""
+
+import pytest
+
+
+def pytest_addoption(parser):
+    """Adds additional options to the pytest commandline."""
+    parser.addoption(
+        "--run-functional", action="store_true", default=False, help="Run functional tests"
+    )
+
+
+def pytest_configure(config):
+    config.addinivalue_line(
+        "markers", "functional: Mark a test as a functional test."
+    )
+
+
+def pytest_collection_modifyitems(config, items):
+    """Updates the collections based on the passed arguments to pytest."""
+    if config.getoption("--run-functional"):
+        return  # If options is passed, don't skip the functional tests.
+    functional_skip_mark = pytest.mark.skip(reason="Needs `--run-functional` to run functional tests.")
+    for item in items:
+        if "functional" in item.keywords:
+            item.add_marker(functional_skip_mark)
