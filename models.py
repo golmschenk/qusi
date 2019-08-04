8b5f15fac75428c47b34e5a1f4b848dd8be2747b
Greg Olmschenk <golmschenk@gmail.com>
2019-08-04 16:43:06 -0400

    Add batch normalization to cube model

--- models.py
+++ models.py
@@ -1,7 +1,7 @@
 """Code for network architectures."""
 from tensorflow import sigmoid
 from tensorflow.python.keras import Sequential
-from tensorflow.python.keras.layers import Conv3D, MaxPool3D, Flatten, Dense, Reshape, LeakyReLU
+from tensorflow.python.keras.layers import Conv3D, MaxPool3D, Flatten, Dense, Reshape, LeakyReLU, BatchNormalization
 from tensorflow.python.keras.regularizers import l2
 
 
@@ -22,19 +22,25 @@ class SimpleCubeCnn(Sequential):
         l2_regularizer = l2(0.001)
         self.add(Conv3D(16, [3, 3, 1], activation=leaky_relu, kernel_regularizer=l2_regularizer))
         self.add(Conv3D(16, [1, 1, 4], activation=leaky_relu, kernel_regularizer=l2_regularizer))
+        self.add(BatchNormalization())
         self.add(MaxPool3D([1, 1, 2]))
         self.add(Conv3D(32, [3, 3, 1], activation=leaky_relu, kernel_regularizer=l2_regularizer))
         self.add(Conv3D(32, [1, 1, 4], activation=leaky_relu, kernel_regularizer=l2_regularizer))
+        self.add(BatchNormalization())
         self.add(MaxPool3D([1, 1, 2]))
         self.add(Conv3D(64, [3, 3, 1], activation=leaky_relu, kernel_regularizer=l2_regularizer))
         self.add(Conv3D(64, [1, 1, 4], activation=leaky_relu, kernel_regularizer=l2_regularizer))
+        self.add(BatchNormalization())
         self.add(MaxPool3D([1, 1, 2]))
         self.add(Conv3D(128, [4, 4, 1], activation=leaky_relu, kernel_regularizer=l2_regularizer))
         self.add(Conv3D(128, [1, 1, 4], activation=leaky_relu, kernel_regularizer=l2_regularizer))
+        self.add(BatchNormalization())
         self.add(MaxPool3D([1, 1, 2]))
         self.add(Conv3D(128, [1, 1, 4], activation=leaky_relu, kernel_regularizer=l2_regularizer))
+        self.add(BatchNormalization())
         self.add(MaxPool3D([1, 1, 2]))
         self.add(Conv3D(128, [1, 1, 4], activation=leaky_relu, kernel_regularizer=l2_regularizer))
+        self.add(BatchNormalization())
         self.add(MaxPool3D([1, 1, 2]))
         self.add(Conv3D(32, [1, 1, 9], activation=leaky_relu, kernel_regularizer=l2_regularizer))
         self.add(Conv3D(16, [1, 1, 1], activation=leaky_relu, kernel_regularizer=l2_regularizer))
