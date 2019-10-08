1a9e7f2c6beea65a591b9faa775ecf50337b4fa0
Greg Olmschenk <golmschenk@gmail.com>
2019-10-08 17:49:55 -0400

    Add inference script for the per-time-step label approach

--- infer.py
+++ infer.py
@@ -1,19 +1,28 @@
 """Code for inference on the contents of a directory."""
-
+import numpy as np
+import pandas as pd
 import tensorflow as tf
+from pathlib import Path
 
-from models import SimpleLightcurveCnn
+from models import ConvolutionalLstm
 from photometric_database.microlensing_label_per_time_step_database import MicrolensingLabelPerTimeStepDatabase
 
-print('Preprocessing data...')
-database = MicrolensingLabelPerTimeStepDatabase()
-example_paths, inference_dataset = database.generate_inference_dataset('data/inference')
+# Set these paths to the correct paths.
+saved_log_directory = Path('logs/baseline YYYY-MM-DD-hh-mm-ss')
+meta_data_path = Path('data/candlist_RADec.dat.feather')
 
-model = SimpleLightcurveCnn()
-model.load_weights('logs/baseline/model.ckpt')
+print('Setting up dataset...')
+database = MicrolensingLabelPerTimeStepDatabase()
+database.meta_data_frame = pd.read_feather(meta_data_path)
+example_paths = pd.read_csv(saved_log_directory.joinpath('validation.csv'))
+
+print('Loading model...')
+model = ConvolutionalLstm()
+model.load_weights(str(saved_log_directory.joinpath('model.ckpt')))
 
 print('Inferring...')
-dataset_tensor = tf.convert_to_tensor(inference_dataset)
-predictions = model.predict(dataset_tensor)
-for index in range(len(predictions)):
-    print(f'{example_paths[index]}: {predictions[index]}')
+for example_path in example_paths:
+    example, label = database.evaluation_preprocessing(tf.convert_to_tensor(example_path))
+    prediction = model.predict(tf.expand_dims(example, axis=0))[0]
+    lightcurve_data_frame = pd.read_feather(example_path)  # Not required for prediction, but useful for analysis.
+    # Use prediction here as desired.
