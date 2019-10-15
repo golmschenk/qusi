340f2ae43354a89ed4ed7dc219946a051c18e5b2
Greg Olmschenk <golmschenk@gmail.com>
2019-10-15 15:18:45 -0400

    Add automatic source code linking on GitHub from the docs

--- docs/conf.py
+++ docs/conf.py
@@ -1,14 +1,15 @@
 """
 Configuration file for the Sphinx documentation builder.
 """
-
+import inspect
 import sys
 import os
 import datetime
 from typing import Dict
-
+from importlib import import_module
 from autoapi.mappers.python.objects import PythonPythonMapper
 from sphinx.application import Sphinx
+from git import Repo
 
 
 # Path setup
@@ -26,7 +27,7 @@ master_doc = 'index'
 
 # General Sphinx configuration
 
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon', 'autoapi.extension']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon', 'autoapi.extension', 'sphinx.ext.linkcode']
 
 add_module_names = False
 templates_path = ['_templates']
@@ -36,14 +37,9 @@ exclude_patterns = ['_build', '_templates', 'Thumbs.db', '.DS_Store']
 # AutoAPI configuration.
 
 autoapi_dirs = ['..']
-autoapi_ignore = ['*envs/*', '*venv/*', '*.pytest_cache/*', '*logs/*', '*data/*', '*docs/conf.py', '*tests/*']
+autoapi_ignore = ['*envs/*', '*venv/*', '*.pytest_cache/*', '*logs/*', '*data/*', '*docs/conf.py', '*docs/build.py',
+                  '*tests/*', '*conftest.py', '*ramjet/train.py', '*ramjet/infer.py']
 autoapi_template_dir = '_templates/autoapi'
-
-
-# HTML output configuration
-
-html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
 
 
 # noinspection PyUnusedLocal
@@ -52,6 +48,57 @@ def autoapi_skip_member(app: Sphinx, what: str, name: str, obj: PythonPythonMapp
     if '__init__' in name:
         return False
     return would_skip
+
+
+# Source code linking.
+
+def linkcode_resolve(domain, info):
+    """
+    Determine the URL corresponding to a Python object.
+    """
+    class PlaceholderException(Exception):
+        """A placeholder exception meant to show where to add specific exceptions in the future for the code linking."""
+        pass
+    if domain != 'py':
+        return None
+    module_name = info['module']
+    full_name = info['fullname']
+    import_module(module_name)
+    module = sys.modules.get(module_name)
+    if module is None:
+        return None
+    object_ = module
+    for part in full_name.split('.'):
+        try:
+            object_ = getattr(object_, part)
+        except AttributeError:
+            return None
+    try:
+        file_name = inspect.getsourcefile(object_)
+    except PlaceholderException:
+        file_name = None
+    if not file_name:
+        return None
+    file_name = os.path.relpath(file_name, start=os.path.abspath('..'))
+    try:
+        source, line_number = inspect.getsourcelines(object_)
+    except PlaceholderException:
+        line_number = None
+        source = None
+    if line_number is not None and source is not None:
+        line_range_jump_option = f'#L{line_number}-L{line_number + len(source) - 1}'
+    else:
+        line_range_jump_option = ''
+    repository = Repo('..')
+    branch = repository.active_branch
+    branch_name = branch.name
+    return f'http://github.com/golmschenk/ramjet/blob/{branch_name}/{file_name}{line_range_jump_option}'
+
+
+# HTML output configuration
+
+html_theme = 'press'
+html_static_path = ['_static']
 
 
 def setup(app: Sphinx):
