1c0c1f2c0f05f69c28d5513b9dc74e263a847012
Greg Olmschenk <golmschenk@gmail.com>
2019-10-23 15:49:10 -0400

    Adds method to generate the meta data required for training on Liang Yu's data

--- photometric_database/liang_yu_lightcurve_database.py
+++ photometric_database/liang_yu_lightcurve_database.py
@@ -14,6 +14,32 @@ class LiangYuLightcurveDatabase(TessTransitLightcurveLabelPerTimeStepDatabase):
     """
     A class to represent the database of TESS data based on `Liang Yu's work <https://arxiv.org/pdf/1904.02726.pdf>`_.
     """
+
+    def __init__(self, data_directory='data/tess'):
+        super().__init__(data_directory=data_directory)
+        self.liang_yu_dispositions_path = self.data_directory.joinpath('liang_yu_dispositions.csv')
+
+    def get_meta_data_frame_for_available_lightcurves(self) -> pd.DataFrame:
+        """
+        Gets the available meta disposition data from Liang Yu's work and combines it with the available lightcurve
+        data, throwing out any data that doesn't have its counterpart.
+
+        :return: The meta data frame containing the lightcurve paths and meta data needed to generate labels.
+        """
+        # noinspection SpellCheckingInspection
+        columns_to_use = ['tic_id', 'Disposition', 'Epoc', 'Period', 'Duration', 'Sectors']
+        liang_yu_dispositions = pd.read_csv(self.liang_yu_dispositions_path, usecols=columns_to_use)
+        # noinspection SpellCheckingInspection
+        liang_yu_dispositions.rename(columns={'Disposition': 'disposition', 'Epoc': 'transit_epoch',
+                                              'Period': 'transit_period', 'Duration': 'transit_duration',
+                                              'Sectors': 'sector'}, inplace=True)
+        lightcurve_paths = list(self.lightcurve_directory.glob('*lc.fits'))
+        tic_ids = [int(self.get_tic_id_from_single_sector_obs_id(path.name)) for path in lightcurve_paths]
+        sectors = [self.get_sector_from_single_sector_obs_id(path.name) for path in lightcurve_paths]
+        lightcurve_meta_data = pd.DataFrame({'lightcurve_path': lightcurve_paths, 'tic_id': tic_ids, 'sector': sectors})
+        meta_data = pd.merge(liang_yu_dispositions, lightcurve_meta_data, how='inner', on=['tic_id', 'sector'])
+        return meta_data
+
     def download_liang_yu_database(self):
         """
         Downloads the database used by https://arxiv.org/pdf/1904.02726.pdf.
@@ -21,8 +47,7 @@ class LiangYuLightcurveDatabase(TessTransitLightcurveLabelPerTimeStepDatabase):
         print("Downloading Liang Yu's disposition CSV...")
         liang_yu_csv_url = 'https://raw.githubusercontent.com/yuliang419/Astronet-Triage/master/astronet/tces.csv'
         response = requests.get(liang_yu_csv_url)
-        liang_yu_disposition_path = self.data_directory.joinpath('liang_yu_disposition.csv')
-        with open(liang_yu_disposition_path, 'wb') as csv_file:
+        with open(self.liang_yu_dispositions_path, 'wb') as csv_file:
             csv_file.write(response.content)
         print('Downloading TESS observation list...')
         tess_observations = self.get_all_tess_time_series_observations()
@@ -32,10 +57,10 @@ class LiangYuLightcurveDatabase(TessTransitLightcurveLabelPerTimeStepDatabase):
         print("Downloading lightcurves which appear in Liang Yu's disposition...")
         # noinspection SpellCheckingInspection
         columns_to_use = ['tic_id', 'Disposition', 'Epoc', 'Period', 'Duration', 'Sectors']
-        liang_yu_disposition = pd.read_csv(liang_yu_disposition_path, usecols=columns_to_use)
-        liang_yu_observations = pd.merge(single_sector_observations, liang_yu_disposition, how='inner',
+        liang_yu_dispositions = pd.read_csv(self.liang_yu_dispositions_path, usecols=columns_to_use)
+        liang_yu_observations = pd.merge(single_sector_observations, liang_yu_dispositions, how='inner',
                                          left_on=['tic_id', 'sector'], right_on=['tic_id', 'Sectors'])
-        number_of_observations_not_found = liang_yu_disposition.shape[0] - liang_yu_observations.shape[0]
+        number_of_observations_not_found = liang_yu_dispositions.shape[0] - liang_yu_observations.shape[0]
         print(f"{liang_yu_observations.shape[0]} observations found that match Liang Yu's entries.")
         print(f'Liang Yu used the FFIs, not the lightcurve products, so many will be missing.')
         print(f"No observations found for {number_of_observations_not_found} entries in Liang Yu's disposition.")
@@ -44,7 +69,7 @@ class LiangYuLightcurveDatabase(TessTransitLightcurveLabelPerTimeStepDatabase):
             liang_yu_data_products['productFilename'].str.endswith('lc.fits')
         ]
         download_manifest = self.download_products(liang_yu_lightcurve_data_products)
-        print('Moving lightcurves to {self.lightcurve_directory}...')
+        print(f'Moving lightcurves to {self.lightcurve_directory}...')
         for file_path_string in download_manifest['Local Path']:
             file_path = Path(file_path_string)
             file_path.rename(self.lightcurve_directory.joinpath(file_path.name))
