07a46d567f6f1a9bdee65fdc011ce007bd3cf56e
golmschenk <golmschenk@gmail.com>
2019-11-29 14:23:57 -0600

    Fix training and validation dataset selections

--- ramjet/photometric_database/lightcurve_database.py
+++ ramjet/photometric_database/lightcurve_database.py
@@ -107,9 +107,14 @@ class LightcurveDatabase(ABC):
                                                             tf.data.Dataset, tf.data.Dataset):
         """Creates a TensorFlow Dataset from a list of file names and desired label for those files."""
         example_paths = [str(example_path) for example_path in example_paths]
-        dataset = tf.data.Dataset.from_tensor_slices(example_paths)
-        validation_dataset_size = int(len(example_paths) * self.validation_ratio)
-        dataset = dataset.shuffle(buffer_size=len(list(dataset)))
-        validation_dataset = dataset.take(validation_dataset_size)
-        training_dataset = dataset.skip(validation_dataset_size)
+        np.random.seed(0)
+        np.random.shuffle(example_paths)
+        number_of_chunks = int(1 / self.validation_ratio)  # In this example, 5 chunks.
+        chunks = np.array_split(example_paths, number_of_chunks)
+        validation_chunk_index = 0  # This value should be changeable to select different validation sets.
+        validation_paths = chunks[validation_chunk_index]
+        training_chunks = np.delete(chunks, validation_chunk_index, axis=0)
+        training_paths = np.concatenate(training_chunks)
+        validation_dataset = tf.data.Dataset.from_tensor_slices(validation_paths)
+        training_dataset = tf.data.Dataset.from_tensor_slices(training_paths)
         return training_dataset, validation_dataset
