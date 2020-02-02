ef7ba389a25fdd532362ef8f7c751eb4cfdf801a
Greg Olmschenk <golmschenk@gmail.com>
2020-02-02 18:42:31 -0500

    Adds a method to created a padded window dataset from a zipped example and label dataset. This combines the features of TensorFlow's standard `padded_batch` and `window` dataset functions

--- tests/photometric_database/test_lightcurve_database.py
+++ tests/photometric_database/test_lightcurve_database.py
@@ -4,6 +4,7 @@ Tests for the LightcurveDatabase class.
 from typing import Any
 from unittest.mock import Mock
 import numpy as np
+import tensorflow as tf
 import pytest
 
 
@@ -31,3 +32,26 @@ class TestLightcurveDatabase:
                                                                          chunk_to_extract_index=1)
         assert np.array_equal(chunk, expected_chunk)
         assert np.array_equal(remainder, expected_remainder)
+
+
+    def test_creating_a_padded_window_dataset_for_a_zipped_example_and_label_dataset(self, database):
+        # noinspection PyMissingOrEmptyDocstring
+        def examples_generator():
+            for example in [[1, 1], [2, 2], [3, 3], [4, 4, 4], [5, 5, 5], [6, 6, 6]]:
+                yield example
+
+        # noinspection PyMissingOrEmptyDocstring
+        def labels_generator():
+            for label in [[-1, -1], [-2, -2], [-3, -3], [-4, -4, -4], [-5, -5, -5], [-6, -6, -6]]:
+                yield label
+
+        example_dataset = tf.data.Dataset.from_generator(examples_generator, output_types=tf.float32)
+        label_dataset = tf.data.Dataset.from_generator(labels_generator, output_types=tf.float32)
+        dataset = tf.data.Dataset.zip((example_dataset, label_dataset))
+        padded_window_dataset = database.padded_window_dataset_for_zipped_example_and_label_dataset(
+            dataset=dataset, batch_size=3, window_shift=2, padded_shapes=([None], [None]))
+        padded_window_iterator = iter(padded_window_dataset)
+        batch0 = next(padded_window_iterator)
+        assert np.array_equal(batch0[0].numpy(), [[1, 1], [2, 2], [3, 3]])
+        batch1 = next(padded_window_iterator)
+        assert np.array_equal(batch1[0].numpy(), [[3, 3, 0], [4, 4, 4], [5, 5, 5]])
