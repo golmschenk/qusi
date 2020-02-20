5ad234b772c8abeb679316fadc79e8db449de734
Greg Olmschenk <golmschenk@gmail.com>
2020-02-20 13:56:56 -0500

    Add end of run messages and short 5 epoch training run

--- train.py
+++ train.py
@@ -15,7 +15,7 @@ def train():
     model = ConvolutionalLstm()
     database = ToiLightcurveDatabase()
     # database.batch_size = 100  # Reducing the batch size may help if you are running out of memory.
-    epochs_to_run = 1000
+    epochs_to_run = 5
     trial_name = 'baseline'
     logs_directory = 'logs'
 
@@ -49,3 +49,6 @@ def train():
 
 if __name__ == '__main__':
     train()
+    print('............')
+    print('... Done ...')
+    print('............')
