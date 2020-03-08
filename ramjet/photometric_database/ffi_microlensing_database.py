e67be743aafc9134e605842b4942bafe9e9fa3f3
Greg Olmschenk <golmschenk@gmail.com>
2020-03-07 19:39:20 -0500

    Add placeholders for FFI microlensing database

---
+++ ramjet/photometric_database/ffi_microlensing_database.py
@@ -0,0 +1,11 @@
+"""
+Code to represent a database to find microlensing events in FFI data.
+"""
+from ramjet.photometric_database.tess_synthetic_injected_database import TessSyntheticInjectedDatabase
+
+
+class FfiMicrolensingDatabase(TessSyntheticInjectedDatabase):
+    """
+    A class to represent a database to find microlensing events in FFI data.
+    """
+    pass
