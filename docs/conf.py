6bcfa0f16bfe9c112c51c55d84b16806f56deabf
Greg Olmschenk <golmschenk@gmail.com>
2020-03-23 19:38:40 -0400

    Update the ignored documentation models to use the change in imported packages

--- docs/conf.py
+++ docs/conf.py
@@ -35,7 +35,7 @@ exclude_patterns = ['_build', '_templates', 'Thumbs.db', '.DS_Store']
 
 # Mock packages we don't expect to be imported for any reason (e.g., requires C libraries on docs VM)
 
-mock_modules = ['muLAn', 'muLAn.models', 'muLAn.models.BLcontU']
+mock_modules = ['muLAn', 'muLAn.models', 'muLAn.models.vbb', 'muLAn.models.vbb.vbb', 'muLAn.models.vbb.vbb.vbbmagU']
 sys.modules.update((module_name, Mock()) for module_name in mock_modules)
 
 # AutoAPI configuration.
