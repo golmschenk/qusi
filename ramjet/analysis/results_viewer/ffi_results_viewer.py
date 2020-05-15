8acf94f51ba9dfd59f672113a1f676a9cd02954d
Greg Olmschenk <golmschenk@gmail.com>
2020-05-14 20:59:35 -0400

    Add placeholder of FFI results viewer

---
+++ ramjet/analysis/results_viewer/ffi_results_viewer.py
@@ -0,0 +1,11 @@
+"""
+A class for viewing FFI inference results.
+"""
+from ramjet.analysis.results_viewer.results_viewer import ResultsViewer
+
+
+class FfiResultsViewer(ResultsViewer):
+    """
+    A viewer inspecting FFI lightcurves of inference candidates.
+    """
+    pass
