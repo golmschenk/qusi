afca62f893845ee3163453e9da4777c820bfa981
Greg Olmschenk <golmschenk@gmail.com>
2020-06-05 19:24:58 -0400

    Remove experiment tests now that the attributes are just type hints

--- tests/test_experiments.py
+++ tests/test_experiments.py
@@ -1,18 +1,6 @@
 """
 Tests for the experiments module.
 """
-from ramjet.experiments import Experiment
-
 
 class TestExperiment:
-    def test_database_attribute_exists(self):
-        # noinspection PyUnresolvedReferences
-        assert 'database' in Experiment.__dict__.keys()
-
-    def test_model_attribute_exists(self):
-        # noinspection PyUnresolvedReferences
-        assert 'model' in Experiment.__dict__.keys()
-
-    def test_run_name_attribute_exists(self):
-        # noinspection PyUnresolvedReferences
-        assert 'run_name' in Experiment.__dict__.keys()
+    pass
