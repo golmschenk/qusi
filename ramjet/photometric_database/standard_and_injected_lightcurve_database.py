ea49a34c54e84bb0351176dffacf447109354809
Greg Olmschenk <golmschenk@gmail.com>
2020-06-16 17:20:52 -0400

    Add method to preprocess an individual standard lightcurve

--- ramjet/photometric_database/standard_and_injected_lightcurve_database.py
+++ ramjet/photometric_database/standard_and_injected_lightcurve_database.py
@@ -1,11 +1,14 @@
 """
 An abstract class allowing for any number and combination of standard and injectable/injectee lightcurve collections.
 """
+import numpy as np
 import tensorflow as tf
-from typing import List, Union
+from pathlib import Path
+from typing import List, Union, Callable, Tuple
 
 from ramjet.photometric_database.lightcurve_collection import LightcurveCollection
 from ramjet.photometric_database.lightcurve_database import LightcurveDatabase
+from ramjet.py_mapper import map_py_function_to_dataset
 
 
 class StandardAndInjectedLightcurveDatabase(LightcurveDatabase):
@@ -23,6 +26,7 @@ class StandardAndInjectedLightcurveDatabase(LightcurveDatabase):
         self.validation_injectee_lightcurve_collection: Union[LightcurveCollection, None] = None
         self.validation_injectable_lightcurve_collections: List[LightcurveCollection] = []
         self.shuffle_buffer_size = 10000
+        self.time_steps_per_example = 20000
 
     def generate_datasets(self) -> (tf.data.Dataset, tf.data.Dataset):
         training_standard_paths_datasets = self.generate_paths_datasets_from_lightcurve_collection_list(
@@ -37,6 +41,35 @@ class StandardAndInjectedLightcurveDatabase(LightcurveDatabase):
             self.validation_injectee_lightcurve_collection)
         validation_injectable_paths_datasets = self.generate_paths_datasets_from_lightcurve_collection_list(
             self.validation_injectable_lightcurve_collections)
+        training_lightcurve_and_label_datasets = []
+        for paths_dataset, lightcurve_collection in zip(training_standard_paths_datasets,
+                                                        self.training_standard_lightcurve_collections):
+            lightcurve_and_label_dataset = self.generate_standard_lightcurve_and_label_dataset(
+                paths_dataset, lightcurve_collection.label
+            )
+            training_lightcurve_and_label_datasets.append(lightcurve_and_label_dataset)
+        for paths_dataset, injectable_lightcurve_collection in zip(training_injectable_paths_datasets,
+                                                                   self.training_injectable_lightcurve_collections):
+            lightcurve_and_label_dataset = self.generate_injected_lightcurve_and_label_dataset(
+                training_injectee_path_dataset, paths_dataset, injectable_lightcurve_collection.label
+            )
+            training_lightcurve_and_label_datasets.append(lightcurve_and_label_dataset)
+        training_dataset = tf.data.Dataset.zip(training_lightcurve_and_label_datasets)
+        validation_lightcurve_and_label_datasets = []
+        for paths_dataset, lightcurve_collection in zip(validation_standard_paths_datasets,
+                                                        self.validation_standard_lightcurve_collections):
+            lightcurve_and_label_dataset = self.generate_standard_lightcurve_and_label_dataset(
+                paths_dataset, lightcurve_collection.label
+            )
+            validation_lightcurve_and_label_datasets.append(lightcurve_and_label_dataset)
+        for paths_dataset, injectable_lightcurve_collection in zip(validation_injectable_paths_datasets,
+                                                                   self.validation_injectable_lightcurve_collections):
+            lightcurve_and_label_dataset = self.generate_injected_lightcurve_and_label_dataset(
+                validation_injectee_path_dataset, paths_dataset, injectable_lightcurve_collection.label
+            )
+            validation_lightcurve_and_label_datasets.append(lightcurve_and_label_dataset)
+        validation_dataset = tf.data.Dataset.zip(validation_lightcurve_and_label_datasets)
+        return training_dataset, validation_dataset
 
     def generate_paths_dataset_from_lightcurve_collection(self, lightcurve_collection: LightcurveCollection
                                                           ) -> tf.data.Dataset:
@@ -61,3 +94,46 @@ class StandardAndInjectedLightcurveDatabase(LightcurveDatabase):
         """
         return [self.generate_paths_dataset_from_lightcurve_collection(lightcurve_collection)
                 for lightcurve_collection in lightcurve_collections]
+
+    def generate_standard_lightcurve_and_label_dataset(
+            self, paths_dataset: tf.data.Dataset,
+            load_from_path_function: Callable[[Path], Tuple[np.ndarray, np.ndarray]], label: float):
+        pass
+        # lightcurve_validation_dataset = map_py_function_to_dataset(zipped_validation_paths_dataset,
+        #                                                            self.positive_injection_negative_injection_and_explicit_negative_injection_preprocessing,
+        #                                                            self.number_of_parallel_processes_per_map,
+        #                                                            output_types=output_types,
+        #                                                            output_shapes=output_shapes,
+        #                                                            flat_map=True)
+
+    def preprocess_standard_lightcurve(self, load_from_path_function: Callable[[Path], Tuple[np.ndarray, np.ndarray]],
+                                       label: float, lightcurve_path_tensor: tf.Tensor) -> (np.ndarray, np.ndarray):
+        """
+        Preprocesses a individual standard lightcurve from a lightcurve path tensor, using a passed function defining
+        how to load the values from the lightcurve file and the label value to use. Designed to be used with `partial`
+        to prepare a function which will just require the lightcurve path tensor, and can then be mapped to a dataset.
+
+        :param load_from_path_function: The function to load the lightcurve times and fluxes from a file.
+        :param label: The label to assign to the lightcurve.
+        :param lightcurve_path_tensor: The tensor containing the path to the lightcurve file.
+        :return: The example and label arrays shaped for use as single example for the network.
+        """
+        lightcurve_path = lightcurve_path_tensor.numpy().decode('utf-8')
+        times, fluxes = load_from_path_function(lightcurve_path)
+        fluxes = self.flux_preprocessing(fluxes)
+        example = np.expand_dims(fluxes, axis=-1)
+        return example, np.array([label])
+
+    def flux_preprocessing(self, fluxes: np.ndarray, evaluation_mode: bool = False, seed: int = None) -> np.ndarray:
+        """
+        Preprocessing for the flux.
+
+        :param fluxes: The flux array to preprocess.
+        :param evaluation_mode: If the preprocessing should be consistent for evaluation.
+        :param seed: Seed for the randomization.
+        :return: The preprocessed flux array.
+        """
+        normalized_fluxes = self.normalize(fluxes)
+        uniform_length_fluxes = self.make_uniform_length(normalized_fluxes, self.time_steps_per_example,
+                                                         randomize=not evaluation_mode, seed=seed)
+        return uniform_length_fluxes
--- tests/photometric_database/test_standard_and_injected_lightcurve_database.py
+++ tests/photometric_database/test_standard_and_injected_lightcurve_database.py
@@ -2,6 +2,7 @@ from unittest.mock import patch
 
 import pytest
 import numpy as np
+import tensorflow as tf
 from pathlib import Path
 
 import ramjet.photometric_database.standard_and_injected_lightcurve_database as database_module
@@ -18,13 +19,13 @@ class TestStandardAndInjectedLightcurveDatabase:
         standard_lightcurve_collection0 = LightcurveCollection(
             lambda: [Path('standard_path0.ext')], lambda path: (np.array([10, 20, 30]), np.array([0, 1, 2])), 0)
         standard_lightcurve_collection1 = LightcurveCollection(
-            lambda: [Path('standard_path1.ext')], lambda path: (np.array([20, 30, 40]), np.array([1, 2, 3])), 0)
+            lambda: [Path('standard_path1.ext')], lambda path: (np.array([20, 30, 40]), np.array([1, 2, 3])), 1)
         injectee_lightcurve_collection = LightcurveCollection(
             lambda: [Path('injectee_path.ext')], lambda path: (np.array([30, 40, 50]), np.array([2, 3, 4])), 0)
         injectable_lightcurve_collection0 = LightcurveCollection(
             lambda: [Path('injectable_path0.ext')], lambda path: (np.array([0, 10, 20]), np.array([0.5, 1, 1.5])), 0)
         injectable_lightcurve_collection1 = LightcurveCollection(
-            lambda: [Path('injectable_path1.ext')], lambda path: (np.array([0, 10, 20, 30]), np.array([0, 1, 1, 0])), 0)
+            lambda: [Path('injectable_path1.ext')], lambda path: (np.array([0, 10, 20, 30]), np.array([0, 1, 1, 0])), 1)
         database.training_standard_lightcurve_collections = [standard_lightcurve_collection0,
                                                              standard_lightcurve_collection1]
         database.training_injectee_lightcurve_collection = injectee_lightcurve_collection
@@ -63,6 +64,32 @@ class TestStandardAndInjectedLightcurveDatabase:
         assert np.array_equal(validation_batch[1].numpy(), [0, 3, 4])  # Injected lightcurve 1, with injectable clipped.
         assert np.array_equal(validation_batch[2].numpy(), [1, 2, 3])  # Standard lightcurve 1.
         assert np.array_equal(validation_batch[3].numpy(), [0, 3, 4])  # Injected lightcurve 1, with injectable clipped.
+
+    @pytest.mark.skip(reason='Working on unit tests that make up the parts of this functional test.')
+    @pytest.mark.slow
+    @pytest.mark.functional
+    def test_can_generate_standard_lightcurve_and_label_dataset_from_paths_dataset_and_label(self, database):
+        lightcurve_collection = database.training_standard_lightcurve_collections[0]
+        paths_dataset = database.generate_paths_dataset_from_lightcurve_collection(lightcurve_collection)
+        label = lightcurve_collection.label
+        lightcurve_and_label_dataset = database.generate_standard_lightcurve_and_label_dataset(paths_dataset, label)
+        lightcurve_and_label = next(iter(lightcurve_and_label_dataset))
+        assert lightcurve_and_label[0].numpy().shape == (3, 1)
+        assert np.array_equal(lightcurve_and_label[0].numpy(), [0, 1, 2])  # Standard lightcurve 0.
+        assert np.array_equal(lightcurve_and_label[1].numpy(), [0])  # Standard label 0.
+
+    def test_can_preprocess_standard_lightcurve(self, database):
+        lightcurve_collection = database.training_standard_lightcurve_collections[0]
+        # noinspection PyUnresolvedReferences
+        lightcurve_path = lightcurve_collection.get_lightcurve_paths()[0]
+        expected_label = lightcurve_collection.label
+        load_from_path_function = lightcurve_collection.load_times_and_fluxes_from_lightcurve_path
+        lightcurve, label = database.preprocess_standard_lightcurve(load_from_path_function,
+                                                                    expected_label,
+                                                                    tf.convert_to_tensor(str(lightcurve_path)))
+        assert lightcurve.shape == (3, 1)
+        assert np.array_equal(lightcurve, [[0], [1], [2]])  # Standard lightcurve 0.
+        assert np.array_equal(label, [expected_label])  # Standard label 0.
 
     def test_can_create_tensorflow_dataset_for_lightcurve_collection_paths(self, database):
         injectee_paths_dataset = database.generate_paths_dataset_from_lightcurve_collection(
