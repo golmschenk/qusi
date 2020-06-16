dcbdb6ba0b8fb722f83665ea783726492dc782c7
Greg Olmschenk <golmschenk@gmail.com>
2020-06-16 13:41:43 -0400

    Add method to produce the path datasets for each of the lightcurve collections in a group

--- ramjet/photometric_database/standard_and_injected_lightcurve_database.py
+++ ramjet/photometric_database/standard_and_injected_lightcurve_database.py
@@ -24,3 +24,16 @@ class StandardAndInjectedLightcurveDatabase(LightcurveDatabase):
 
     def generate_datasets(self) -> (tf.data.Dataset, tf.data.Dataset):
         pass
+
+    def generate_paths_datasets_from_lightcurve_collection_group(
+                self, standard_lightcurve_collections: List[LightcurveCollection],
+                injectee_lightcurve_collection: LightcurveCollection,
+                injectable_lightcurve_collections: List[LightcurveCollection]
+            ) -> (List[tf.data.Dataset], tf.data.Dataset, List[tf.data.Dataset]):
+        standard_paths_datasets = [self.paths_dataset_from_list_or_generator_factory(collection.get_lightcurve_paths)
+                                   for collection in standard_lightcurve_collections]
+        injectee_path_dataset = self.paths_dataset_from_list_or_generator_factory(
+            injectee_lightcurve_collection.get_lightcurve_paths)
+        injectable_paths_datasets = [self.paths_dataset_from_list_or_generator_factory(collection.get_lightcurve_paths)
+                                   for collection in injectable_lightcurve_collections]
+        return standard_paths_datasets, injectee_path_dataset, injectable_paths_datasets
--- tests/photometric_database/test_standard_and_injected_lightcurve_database.py
+++ tests/photometric_database/test_standard_and_injected_lightcurve_database.py
@@ -7,17 +7,9 @@ from ramjet.photometric_database.standard_and_injected_lightcurve_database impor
 
 
 class TestStandardAndInjectedLightcurveDatabase:
-    def test_database_has_lightcurve_collection_properties(self):
-        database = StandardAndInjectedLightcurveDatabase()
-        assert hasattr(database, 'training_standard_lightcurve_collections')
-        assert hasattr(database, 'training_injectee_lightcurve_collection')
-        assert hasattr(database, 'training_injectable_lightcurve_collections')
-        assert hasattr(database, 'validation_standard_lightcurve_collections')
-        assert hasattr(database, 'validation_injectee_lightcurve_collection')
-        assert hasattr(database, 'validation_injectable_lightcurve_collections')
-
-    @pytest.mark.functional
-    def test_database_can_generate_training_and_validation_datasets(self):
+    @pytest.fixture
+    def database(self) -> StandardAndInjectedLightcurveDatabase:
+        """A fixture of the database with lightcurve collections pre-prepared"""
         database = StandardAndInjectedLightcurveDatabase()
         # Setup mock lightcurve collections.
         standard_lightcurve_collection0 = LightcurveCollection(
@@ -42,14 +34,40 @@ class TestStandardAndInjectedLightcurveDatabase:
         database.batch_size = 4
         database.time_steps_per_example = 3
         database.normalize = lambda fluxes: fluxes  # Don't normalize values to keep it simple.
+        return database
+
+    def test_database_has_lightcurve_collection_properties(self):
+        database = StandardAndInjectedLightcurveDatabase()
+        assert hasattr(database, 'training_standard_lightcurve_collections')
+        assert hasattr(database, 'training_injectee_lightcurve_collection')
+        assert hasattr(database, 'training_injectable_lightcurve_collections')
+        assert hasattr(database, 'validation_standard_lightcurve_collections')
+        assert hasattr(database, 'validation_injectee_lightcurve_collection')
+        assert hasattr(database, 'validation_injectable_lightcurve_collections')
+
+    @pytest.mark.functional
+    def test_database_can_generate_training_and_validation_datasets(self, database):
         training_dataset, validation_dataset = database.generate_datasets()
-        training_batch = next(training_dataset)
+        training_batch = next(iter(training_dataset))
         assert np.array_equal(training_batch[0].numpy(), [0, 1, 2])  # Standard lightcurve 0.
         assert np.array_equal(training_batch[1].numpy(), [1, 2, 3])  # Standard lightcurve 1.
         assert np.array_equal(training_batch[2].numpy(), [1, 3, 6])  # Injected lightcurve 0.
         assert np.array_equal(training_batch[3].numpy(), [0, 3, 4])  # Injected lightcurve 1, with injectable clipped.
-        validation_batch = next(validation_dataset)
+        validation_batch = next(iter(validation_dataset))
         assert np.array_equal(validation_batch[0].numpy(), [1, 2, 3])  # Standard lightcurve 1.
         assert np.array_equal(validation_batch[1].numpy(), [0, 3, 4])  # Injected lightcurve 1, with injectable clipped.
         assert np.array_equal(validation_batch[2].numpy(), [1, 2, 3])  # Standard lightcurve 1.
         assert np.array_equal(validation_batch[3].numpy(), [0, 3, 4])  # Injected lightcurve 1, with injectable clipped.
+
+    def test_database_will_create_a_tensorflow_dataset_for_each_lightcurve_path_collection(self, database):
+        paths_dataset_groups = database.generate_paths_datasets_from_lightcurve_collection_group(
+            database.training_standard_lightcurve_collections,
+            database.training_injectee_lightcurve_collection,
+            database.training_injectable_lightcurve_collections
+        )
+        standard_paths_datasets, injectee_path_dataset, injectable_paths_datasets = paths_dataset_groups
+        assert next(iter(standard_paths_datasets[0])).numpy() == b'standard_path0.ext'
+        assert next(iter(standard_paths_datasets[1])).numpy() == b'standard_path1.ext'
+        assert next(iter(injectee_path_dataset)).numpy() == b'injectee_path.ext'
+        assert next(iter(injectable_paths_datasets[0])).numpy() == b'injectable_path0.ext'
+        assert next(iter(injectable_paths_datasets[1])).numpy() == b'injectable_path1.ext'
