9133cad550e91de57d6a47c9b1b3ed8d47696e80
Greg Olmschenk <golmschenk@gmail.com>
2020-06-23 14:10:33 -0400

    Add a database to train a network to find self lensing binaries in TESS two minute cadence data

--- ramjet/photometric_database/derived/self_lensing_binary_database.py
+++ ramjet/photometric_database/derived/self_lensing_binary_database.py
@@ -1,2 +1,21 @@
-class SelfLensingBinaryDatabase():
-    pass
+from ramjet.photometric_database.derived.self_lensing_binary_synthetic_signals_lightcurve_collection import \
+    SelfLensingBinarySyntheticSignalsLightcurveCollection, ReversedSelfLensingBinarySyntheticSignalsLightcurveCollection
+from ramjet.photometric_database.derived.tess_two_minute_lightcurve_collection import TessTwoMinuteLightcurveCollection
+from ramjet.photometric_database.standard_and_injected_lightcurve_database import StandardAndInjectedLightcurveDatabase
+
+
+class SelfLensingBinaryDatabase(StandardAndInjectedLightcurveDatabase):
+    """
+    A database to train a network to find self lensing binaries in TESS two minute cadence data.
+    """
+    def __init__(self):
+        super().__init__()
+        self.training_standard_lightcurve_collections = [TessTwoMinuteLightcurveCollection()]
+        self.training_injectee_lightcurve_collection = TessTwoMinuteLightcurveCollection()
+        self.training_injectable_lightcurve_collections = [
+            SelfLensingBinarySyntheticSignalsLightcurveCollection(),
+            ReversedSelfLensingBinarySyntheticSignalsLightcurveCollection()
+        ]
+        self.validation_standard_lightcurve_collections = self.training_standard_lightcurve_collections
+        self.validation_injectee_lightcurve_collection = self.training_injectee_lightcurve_collection
+        self.validation_injectable_lightcurve_collections = self.training_injectable_lightcurve_collections
