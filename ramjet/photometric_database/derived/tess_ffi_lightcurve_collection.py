c6e35321a581056b11270fe43e91989945f746d3
Greg Olmschenk <golmschenk@gmail.com>
2020-06-24 15:15:50 -0400

    Add TESS FFI lightcurve collection

--- ramjet/photometric_database/derived/tess_ffi_lightcurve_collection.py
+++ ramjet/photometric_database/derived/tess_ffi_lightcurve_collection.py
@@ -1,20 +1,20 @@
 """
-Code for a lightcurve collection of the TESS two minute cadence data.
+Code for a lightcurve collection of the TESS FFI data, as produced by Brian Powell.
 """
 import numpy as np
 from pathlib import Path
 from typing import Iterable
 
-from ramjet.data_interface.tess_data_interface import TessDataInterface
+from ramjet.data_interface.tess_ffi_data_interface import TessFfiDataInterface
 from ramjet.photometric_database.lightcurve_collection import LightcurveCollection
 
 
-tess_data_interface = TessDataInterface()
+tess_ffi_data_interface = TessFfiDataInterface()
 
 
-class TessTwoMinuteCadenceLightcurveCollection(LightcurveCollection):
+class TessFfiLightcurveCollection(LightcurveCollection):
     """
-    A lightcurve collection of the TESS two minute cadence data.
+    A lightcurve collection of the TESS FFI data, as produced by Brian Powell.
     """
     def __init__(self):
         super().__init__()
@@ -26,7 +26,7 @@ class TessTwoMinuteCadenceLightcurveCollection(LightcurveCollection):
 
         :return: An iterable of the lightcurve paths.
         """
-        return Path('data/tess_two_minute_cadence_lightcurves').glob('*.fits')
+        return tess_ffi_data_interface.paths_generator_from_sql_table()
 
     def load_times_and_fluxes_from_path(self, path: Path) -> (np.ndarray, np.ndarray):
         """
@@ -35,7 +35,7 @@ class TessTwoMinuteCadenceLightcurveCollection(LightcurveCollection):
         :param path: The path to the lightcurve file.
         :return: The times and the fluxes of the lightcurve.
         """
-        fluxes, times = tess_data_interface.load_fluxes_and_times_from_fits_file(path)
+        fluxes, times = tess_ffi_data_interface.load_fluxes_and_times_from_pickle_file(path)
         return times, fluxes
 
     def load_times_and_magnifications_from_path(self, path: Path) -> (np.ndarray, np.ndarray):
@@ -45,6 +45,6 @@ class TessTwoMinuteCadenceLightcurveCollection(LightcurveCollection):
         :param path: The path to the lightcurve/signal file.
         :return: The times and the magnifications of the lightcurve/signal.
         """
-        fluxes, times = tess_data_interface.load_fluxes_and_times_from_fits_file(path)
+        fluxes, times = tess_ffi_data_interface.load_fluxes_and_times_from_pickle_file(path)
         magnifications, times = self.generate_synthetic_signal_from_real_data(fluxes, times)
         return times, magnifications
