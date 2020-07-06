02675d0df81610dc70646af5b6a9c089b81735a4
stelais <stela.ishitani@gmail.com>
2020-07-05 22:22:00 -0400

    Creating the MOA lightcurve collection. Separate classes to use real microlensing data or synthetic one.

---
+++ ramjet/photometric_database/derived/moa_microlensing_lightcurve_collection.py
@@ -0,0 +1,116 @@
+"""
+Code for a lightcurve collection of the MOA data.
+"""
+from pathlib import Path
+from typing import Iterable
+import numpy as np
+import pandas as pd
+
+from ramjet.photometric_database.lightcurve_collection import LightcurveCollection
+from ramjet.photometric_database.microlensing_signal_generator import MagnificationSignal
+
+
+class MOAPositiveMicrolensingLightcurveCollection(LightcurveCollection):
+    """
+    A lightcurve collection of the MOA data with previously detected microlensing events.
+    """
+    def __init__(self):
+        super().__init__()
+        self.label = 1
+
+    def get_paths(self) -> Iterable[Path]:
+        """
+        Gets the paths for the lightcurves in the positive collection.
+
+        :return: An iterable of the lightcurve paths.
+        """
+        paths = Path('/local/data/fugu3/sishitan/ramjet/data/moa_microlensing/positive').glob('*.feather')
+        return paths
+
+    def load_times_and_fluxes_from_path(self, path: Path) -> (np.ndarray, np.ndarray):
+        """
+        Loads the times and fluxes from a given lightcurve path.
+
+        :param path: The path to the lightcurve file.
+        :return: The times and the fluxes of the lightcurve.
+        """
+        lightcurve_dataframe = pd.read_feather(path)
+        times = lightcurve_dataframe['Time'].values
+        fluxes = lightcurve_dataframe['Flux'].values
+        return times, fluxes
+
+
+class MOANegativeMicrolensingLightcurveCollection(LightcurveCollection):
+    """
+    A lightcurve collection of the MOA data with no microlensing event.
+    """
+    def __init__(self):
+        super().__init__()
+        self.label = 0
+
+    def get_paths(self):
+        """
+        Gets the paths for the lightcurves in the negative collection.
+
+        :return: An iterable of the lightcurve paths.
+        """
+        paths = Path('/local/data/fugu3/sishitan/ramjet/data/moa_microlensing/negative').glob('*.feather')
+        return paths
+
+    def load_times_and_fluxes_from_path(self, path: Path) -> (np.ndarray, np.ndarray):
+        lightcurve_dataframe = pd.read_feather(path)
+        times = lightcurve_dataframe['Time'].values
+        fluxes = lightcurve_dataframe['Flux'].values
+        return times, fluxes
+
+
+class MicrolensingSyntheticPSPLSignalCollection(LightcurveCollection):
+    def __init__(self):
+        super().__init__()
+        self.label = 1
+
+    def get_paths(self):
+        """
+        Gets the paths for the PSPL microlensing signals.
+
+        :return: An iterable of the lightcurve paths.
+        """
+        paths = Path('/local/data/fugu3/sishitan/muLAn_project/PSPL').glob('*.feather')
+        return paths
+
+    def load_times_and_magnifications_from_path(self, path: Path) -> (np.ndarray, np.ndarray):
+        """
+        Loads the times and magnifications from a given path as an injectable signal.
+
+        :param path: The path to the lightcurve/signal file.
+        :return: The times and the magnifications of the lightcurve/signal.
+        """
+        signal_dataframe = pd.read_feather(path)
+        times = signal_dataframe['Time'].values
+        magnifications = signal_dataframe['Magnification'].values
+        return times, magnifications
+
+
+class MicrolensingSyntheticGeneratedDuringRunningLightcurveCollection(LightcurveCollection):
+    def __init__(self):
+        super().__init__()
+        self.label = 1
+
+    def get_paths(self) -> Iterable[Path]:
+        """
+        No need to get paths because this function will generate the signals on the fly.
+
+        :return: empty generator.
+        """
+
+        return [Path('')]
+
+    def load_times_and_magnifications_from_path(self, path: Path) -> (np.ndarray, np.ndarray):
+        """
+        Loads the times and magnifications from a random generated signal.
+
+        :param path: empty path
+        :return: The times and the magnifications of the signal.
+        """
+        random_signal = MagnificationSignal.generate_randomly_based_on_moa_observations()
+        return random_signal.timeseries, random_signal.magnification
