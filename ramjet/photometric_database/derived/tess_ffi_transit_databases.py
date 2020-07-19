4a8ca5c072a66eb418926ef825cf0235ce49c965
Greg Olmschenk <golmschenk@gmail.com>
2020-07-19 14:40:39 -0400

    Add the basic FFI transit databases

---
+++ ramjet/photometric_database/derived/tess_ffi_transit_databases.py
@@ -0,0 +1,62 @@
+from ramjet.photometric_database.derived.tess_ffi_transit_lightcurve_collections import \
+    TessFfiConfirmedAndCandidateTransitLightcurveCollection, TessFfiNonTransitLightcurveCollection
+from ramjet.photometric_database.standard_and_injected_lightcurve_database import StandardAndInjectedLightcurveDatabase, \
+    OutOfBoundsInjectionHandlingMethod
+
+
+class TessFfiStandardTransitDatabase(StandardAndInjectedLightcurveDatabase):
+    """
+    A database using standard positive and negative transit lightcurves.
+    """
+    def __init__(self):
+        super().__init__()
+        self.training_standard_lightcurve_collections = [
+            TessFfiConfirmedAndCandidateTransitLightcurveCollection(dataset_splits=list(range(8))),
+            TessFfiNonTransitLightcurveCollection(dataset_splits=list(range(8)))
+        ]
+        self.validation_standard_lightcurve_collections = [
+            TessFfiConfirmedAndCandidateTransitLightcurveCollection(dataset_splits=[8]),
+            TessFfiNonTransitLightcurveCollection(dataset_splits=[8])
+        ]
+
+
+class TessFfiInjectedTransitDatabase(StandardAndInjectedLightcurveDatabase):
+    """
+    A database using positive and negative transit lightcurves injected into negative lightcurves.
+    """
+    def __init__(self):
+        super().__init__()
+        self.out_of_bounds_injection_handling = OutOfBoundsInjectionHandlingMethod.REPEAT_SIGNAL
+        self.training_injectee_lightcurve_collection = TessFfiNonTransitLightcurveCollection(
+            dataset_splits=list(range(8)))
+        self.training_injectable_lightcurve_collections = [
+            TessFfiConfirmedAndCandidateTransitLightcurveCollection(dataset_splits=list(range(8))),
+            TessFfiNonTransitLightcurveCollection(dataset_splits=list(range(8)))
+        ]
+        self.validation_standard_lightcurve_collections = [
+            TessFfiConfirmedAndCandidateTransitLightcurveCollection(dataset_splits=[8]),
+            TessFfiNonTransitLightcurveCollection(dataset_splits=[8])
+        ]
+
+
+class TessFfiStandardAndInjectedTransitDatabase(StandardAndInjectedLightcurveDatabase):
+    """
+    A database using positive and negative transit lightcurves injected into negative lightcurves.
+    """
+    def __init__(self):
+        super().__init__()
+        self.out_of_bounds_injection_handling = OutOfBoundsInjectionHandlingMethod.REPEAT_SIGNAL
+        self.training_standard_lightcurve_collections = [
+            TessFfiConfirmedAndCandidateTransitLightcurveCollection(dataset_splits=list(range(8))),
+            TessFfiNonTransitLightcurveCollection(dataset_splits=list(range(8)))
+        ]
+        self.training_injectee_lightcurve_collection = TessFfiNonTransitLightcurveCollection(
+            dataset_splits=list(range(8)))
+        self.training_injectable_lightcurve_collections = [
+            TessFfiConfirmedAndCandidateTransitLightcurveCollection(dataset_splits=list(range(8))),
+            TessFfiNonTransitLightcurveCollection(dataset_splits=list(range(8)))
+        ]
+        self.validation_standard_lightcurve_collections = [
+            TessFfiConfirmedAndCandidateTransitLightcurveCollection(dataset_splits=[8]),
+            TessFfiNonTransitLightcurveCollection(dataset_splits=[8])
+        ]
