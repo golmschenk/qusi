e730f7d1b53e58fa837905859b7c6428104318e3
Greg Olmschenk <golmschenk@gmail.com>
2020-07-22 11:12:37 -0400

    Add TESS metadata manager preparation to quick start tutorial data download

--- ramjet/photometric_database/tutorial/quick_start.py
+++ ramjet/photometric_database/tutorial/quick_start.py
@@ -4,6 +4,7 @@ Code to download and prepare the data needed for the quick start tutorial.
 from pathlib import Path
 
 from ramjet.data_interface.tess_data_interface import TessDataInterface
+from ramjet.data_interface.tess_target_metadata_manager import TessTargetMetadataManger
 from ramjet.data_interface.tess_toi_data_interface import TessToiDataInterface
 from ramjet.data_interface.tess_transit_metadata_manager import TessTransitMetadataManager
 from ramjet.data_interface.tess_two_minute_cadence_lightcurve_metadata_manager import \
@@ -20,10 +21,12 @@ def download():
     tess_toi_data_interface = TessToiDataInterface()
     tess_toi_data_interface.download_exofop_toi_lightcurves_to_directory(
         Path('data/tess_two_minute_cadence_lightcurves'))
-    manager = TessTwoMinuteCadenceLightcurveMetadataManger()
-    manager.build_table()
+    tess_two_minute_cadence_lightcurve_metadata_manger = TessTwoMinuteCadenceLightcurveMetadataManger()
+    tess_two_minute_cadence_lightcurve_metadata_manger.build_table()
     tess_transit_metadata_manager = TessTransitMetadataManager()
     tess_transit_metadata_manager.build_table()
+    tess_target_metadata_manger = TessTargetMetadataManger()
+    tess_target_metadata_manger.build_table()
 
 
 if __name__ == '__main__':
