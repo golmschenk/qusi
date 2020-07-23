e95cf9ee0242dbb0c773bca490283b37dba3c7fe
Greg Olmschenk <golmschenk@gmail.com>
2020-07-23 15:42:37 -0400

    Add docstrings for the infer code.

--- ramjet/trial.py
+++ ramjet/trial.py
@@ -1,3 +1,6 @@
+"""
+Boilerplate code for running trials.
+"""
 
 import io
 import numpy as np
@@ -5,8 +8,17 @@ import pandas as pd
 import tensorflow as tf
 from pathlib import Path
 
+
 def infer(model: tf.keras.Model, dataset: tf.data.Dataset, infer_results_path: Path,
           number_of_top_predictions_to_keep: int = None):
+    """
+    Performs inference of a model on a dataset saving the results to a file.
+
+    :param model: The model to infer with.
+    :param dataset: The dataset to infer on.
+    :param infer_results_path: The path to save the resulting predictions to.
+    :param number_of_top_predictions_to_keep: The number of top results to keep. None will save all results.
+    """
     columns = ['Lightcurve path', 'Prediction']
     dtypes = [str, int]
     predictions_data_frame = pd.read_csv(io.StringIO(''), names=columns, dtype=dict(zip(columns, dtypes)))
@@ -26,6 +38,14 @@ def infer(model: tf.keras.Model, dataset: tf.data.Dataset, infer_results_path: P
 
 def save_results(predictions_data_frame: pd.DataFrame, infer_results_path: Path,
                  number_of_top_predictions_to_keep: int = None):
+    """
+    Saves a predictions data frame to a file.
+
+    :param predictions_data_frame: The data frame of predictions to save.
+    :param infer_results_path: The path to save the resulting predictions to.
+    :param number_of_top_predictions_to_keep: The number of top results to keep. None will save all results.
+    :return:
+    """
     predictions_data_frame = predictions_data_frame.sort_values('Prediction', ascending=False)
     if number_of_top_predictions_to_keep is not None:
         predictions_data_frame = predictions_data_frame.head(number_of_top_predictions_to_keep)
