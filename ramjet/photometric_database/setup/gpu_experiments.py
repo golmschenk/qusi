52c5b9a9eeccd9111dd2957b3b51e011c0eee19e
Greg Olmschenk <golmschenk@gmail.com>
2020-07-23 17:20:49 -0400

    Remove TOI specific download from GPU experiments setup, as the total 2 minute dataset call includes those

--- ramjet/photometric_database/setup/gpu_experiments.py
+++ ramjet/photometric_database/setup/gpu_experiments.py
@@ -14,9 +14,6 @@ def download():
     """
     tess_data_interface = TessDataInterface()
     tess_data_interface.download_two_minute_cadence_lightcurves(Path('data/tess_two_minute_cadence_lightcurves'))
-    tess_toi_data_interface = TessToiDataInterface()
-    tess_toi_data_interface.download_exofop_toi_lightcurves_to_directory(
-        Path('data/tess_two_minute_cadence_lightcurves'))
     build_tables()
 
 
