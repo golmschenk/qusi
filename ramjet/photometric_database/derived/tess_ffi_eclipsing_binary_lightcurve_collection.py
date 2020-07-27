3fb77573f4fff0d618532e4e04ad904092ad4e01
Greg Olmschenk <golmschenk@gmail.com>
2020-07-27 11:46:00 -0400

    Exclude known transit TIC IDs from the negative eclipsing binary training for transit hunting

--- ramjet/photometric_database/derived/tess_ffi_eclipsing_binary_lightcurve_collection.py
+++ ramjet/photometric_database/derived/tess_ffi_eclipsing_binary_lightcurve_collection.py
@@ -7,6 +7,7 @@ from peewee import Select
 
 from ramjet.data_interface.tess_eclipsing_binary_metadata_manager import TessEclipsingBinaryMetadata
 from ramjet.data_interface.tess_ffi_lightcurve_metadata_manager import TessFfiLightcurveMetadata
+from ramjet.data_interface.tess_transit_metadata_manager import TessTransitMetadata, Disposition as TransitDisposition
 from ramjet.photometric_database.derived.tess_ffi_lightcurve_collection import TessFfiLightcurveCollection
 
 
@@ -31,12 +32,27 @@ class TessFfiEclipsingBinaryLightcurveCollection(TessFfiLightcurveCollection):
         return query
 
 
-class TessFfiEclipsingBinaryNegativeLabelLightcurveCollection(TessFfiEclipsingBinaryLightcurveCollection):
+class TessFfiAntiEclipsingBinaryForTransitLightcurveCollection(TessFfiLightcurveCollection):
     """
-    A class representing the collection of TESS two minute cadence lightcurves containing eclipsing binaries with a
-    negative label.
+    A class representing the collection of TESS two minute cadence lightcurves flagged as eclipsing binaries which are
+    not a suspected transit.
     """
     def __init__(self, dataset_splits: Union[List[int], None] = None,
                  magnitude_range: (Union[float, None], Union[float, None]) = (None, None)):
         super().__init__(dataset_splits=dataset_splits, magnitude_range=magnitude_range)
         self.label = 0
+
+    def get_sql_query(self) -> Select:
+        """
+        Gets the SQL query for the database models for the lightcurve collection.
+
+        :return: The SQL query.
+        """
+        query = super().get_sql_query()
+        transit_tic_id_query = TessTransitMetadata.select(TessTransitMetadata.tic_id).where(
+            (TessTransitMetadata.disposition == TransitDisposition.CONFIRMED.value) |
+            (TessTransitMetadata.disposition == TransitDisposition.CANDIDATE.value))
+        eclipsing_binary_tic_id_query = TessEclipsingBinaryMetadata.select(TessEclipsingBinaryMetadata.tic_id).where(
+            TessEclipsingBinaryMetadata.tic_id.not_in(transit_tic_id_query))
+        query = query.where(TessFfiLightcurveMetadata.tic_id.in_(eclipsing_binary_tic_id_query))
+        return query
