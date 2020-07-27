d1fd45a537035a7a6d32e0c6ecb43ff715719cff
Greg Olmschenk <golmschenk@gmail.com>
2020-07-26 23:52:04 -0400

    Add the anti EB FFI transit database

--- ramjet/photometric_database/derived/tess_ffi_transit_databases.py
+++ ramjet/photometric_database/derived/tess_ffi_transit_databases.py
@@ -1,3 +1,5 @@
+from ramjet.photometric_database.derived.tess_ffi_eclipsing_binary_lightcurve_collection import \
+    TessFfiEclipsingBinaryNegativeLabelLightcurveCollection
 from ramjet.photometric_database.derived.tess_ffi_transit_lightcurve_collections import \
     TessFfiConfirmedAndCandidateTransitLightcurveCollection, TessFfiNonTransitLightcurveCollection
 from ramjet.photometric_database.standard_and_injected_lightcurve_database import StandardAndInjectedLightcurveDatabase, \
@@ -19,6 +21,26 @@ class TessFfiStandardTransitDatabase(StandardAndInjectedLightcurveDatabase):
         self.validation_standard_lightcurve_collections = [
             TessFfiConfirmedAndCandidateTransitLightcurveCollection(dataset_splits=[8]),
             TessFfiNonTransitLightcurveCollection(dataset_splits=[8])
+        ]
+
+
+class TessFfiStandardTransitAntiEclipsingBinaryDatabase(StandardAndInjectedLightcurveDatabase):
+    """
+    A database using standard positive and negative transit lightcurves and a negative eclipsing binary collection.
+    """
+    def __init__(self):
+        super().__init__()
+        self.batch_size = 1000
+        self.time_steps_per_example = 1296  # 27 days / 30 minutes.
+        self.training_standard_lightcurve_collections = [
+            TessFfiConfirmedAndCandidateTransitLightcurveCollection(dataset_splits=list(range(8))),
+            TessFfiNonTransitLightcurveCollection(dataset_splits=list(range(8))),
+            TessFfiEclipsingBinaryNegativeLabelLightcurveCollection(dataset_splits=list(range(8)))
+        ]
+        self.validation_standard_lightcurve_collections = [
+            TessFfiConfirmedAndCandidateTransitLightcurveCollection(dataset_splits=[8]),
+            TessFfiNonTransitLightcurveCollection(dataset_splits=[8]),
+            TessFfiEclipsingBinaryNegativeLabelLightcurveCollection(dataset_splits=[8])
         ]
 
 
