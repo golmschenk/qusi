5296f92cc0e455392bdac06942e4e9706b4d64df
Greg Olmschenk <golmschenk@gmail.com>
2020-08-04 11:42:44 -0400

    Remove UUID ordering and replace with dataset_split ordering

--- ramjet/photometric_database/sql_metadata_lightcurve_collection.py
+++ ramjet/photometric_database/sql_metadata_lightcurve_collection.py
@@ -1,8 +1,9 @@
 """
 Code for a lightcurve collection that stores its metadata in the SQL database.
 """
+import random
 from pathlib import Path
-from typing import Iterable
+from typing import Iterable, List
 from uuid import uuid4
 
 from peewee import Select, Field, Case
@@ -65,4 +66,22 @@ class SqlMetadataLightcurveCollection(LightcurveCollection):
         """
         random_start_case = Case(None, [(uuid_field > uuid4(), 0)], 1)
         updated_select_query = select_query.order_by(random_start_case, uuid_field)
+        return updated_select_query
+
+    @staticmethod
+    def order_by_dataset_split_with_random_start(select_query: Select, dataset_split_field: Field,
+                                                 available_dataset_splits: List[int]) -> Select:
+        """
+        Applies an "order by" on a query using a passed dataset_split field. The "order by" starts at a random
+        dataset_split out of the passed available options, then loops back to the minimum dataset_split to include all
+        entities.
+
+        :param select_query: The query to add the "order by" to.
+        :param dataset_split_field: The dataset_split field to order on.
+        :param available_dataset_splits: The available dataset_splits to start on.
+        :return: The query updated to include the "order by".
+        """
+        start_dataset_split = random.choice(available_dataset_splits)
+        start_case = Case(None, [(dataset_split_field >= start_dataset_split, 0)], 1)
+        updated_select_query = select_query.order_by(start_case, dataset_split_field)
         return updated_select_query
