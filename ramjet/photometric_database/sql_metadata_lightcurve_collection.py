716656d38eb6f5f26b2130db0ac90f0b8de4a726
Greg Olmschenk <golmschenk@gmail.com>
2020-08-10 16:48:03 -0400

    Set the dataset split choices if set to None

--- ramjet/photometric_database/sql_metadata_lightcurve_collection.py
+++ ramjet/photometric_database/sql_metadata_lightcurve_collection.py
@@ -3,7 +3,7 @@ Code for a lightcurve collection that stores its metadata in the SQL database.
 """
 import random
 from pathlib import Path
-from typing import Iterable, List
+from typing import Iterable, List, Union
 from uuid import uuid4
 
 from peewee import Select, Field, Case
@@ -71,7 +71,7 @@ class SqlMetadataLightcurveCollection(LightcurveCollection):
 
     @staticmethod
     def order_by_dataset_split_with_random_start(select_query: Select, dataset_split_field: Field,
-                                                 available_dataset_splits: List[int]) -> Select:
+                                                 available_dataset_splits: Union[List[int], None]) -> Select:
         """
         Applies an "order by" on a query using a passed dataset_split field. The "order by" starts at a random
         dataset_split out of the passed available options, then loops back to the minimum dataset_split to include all
@@ -82,6 +82,8 @@ class SqlMetadataLightcurveCollection(LightcurveCollection):
         :param available_dataset_splits: The available dataset_splits to start on.
         :return: The query updated to include the "order by".
         """
+        if available_dataset_splits is None:
+            available_dataset_splits = list(range(10))
         start_dataset_split = random.choice(available_dataset_splits)
         start_case = Case(None, [(dataset_split_field >= start_dataset_split, 0)], 1)
         updated_select_query = select_query.order_by(start_case, dataset_split_field)
