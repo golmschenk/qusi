90d63e6897b3b3ca2754674ea5aa6c940675164b
Greg Olmschenk <golmschenk@gmail.com>
2020-09-04 12:50:24 -0400

    Fix tests using the FITS index enum vs the column name enum

--- tests/photometric_database/test_tess_two_minute_cadence_light_curve.py
+++ tests/photometric_database/test_tess_two_minute_cadence_light_curve.py
@@ -13,11 +13,11 @@ from ramjet.photometric_database.tess_two_minute_cadence_light_curve import Tess
 class TestTessTwoMinuteCadenceFileBasedLightCurve:
     @pytest.fixture
     def fake_hdu_list(self):
-        mock_hdu_data = {TessTwoMinuteCadenceColumnName.TIME.value: [0, 1],
-                         TessTwoMinuteCadenceColumnName.PDCSAP_FLUX.value: [2, 3],
-                         TessTwoMinuteCadenceColumnName.SAP_FLUX.value: [4, 5],
-                         TessTwoMinuteCadenceColumnName.PDCSAP_FLUX_ERROR.value: [6, 7],
-                         TessTwoMinuteCadenceColumnName.SAP_FLUX_ERROR.value: [8, 9]}
+        mock_hdu_data = {TessTwoMinuteCadenceMastFitsIndex.TIME.value: [0, 1],
+                         TessTwoMinuteCadenceMastFitsIndex.PDCSAP_FLUX.value: [2, 3],
+                         TessTwoMinuteCadenceMastFitsIndex.SAP_FLUX.value: [4, 5],
+                         TessTwoMinuteCadenceMastFitsIndex.PDCSAP_FLUX_ERROR.value: [6, 7],
+                         TessTwoMinuteCadenceMastFitsIndex.SAP_FLUX_ERROR.value: [8, 9]}
         mock_hdu = Mock()
         mock_hdu.data = mock_hdu_data
         mock_hdu_list = [None, mock_hdu]  # TESS light curve data is in index 1 of the HDU list.
@@ -33,15 +33,16 @@ class TestTessTwoMinuteCadenceFileBasedLightCurve:
             mock_open.return_value.__enter__.return_value = fake_hdu_list
             light_curve = TessTwoMinuteCadenceLightCurve.from_path(Path('TIC 169480782 sector 5.fits'))
             expected_data_frame = pd.DataFrame(fake_hdu_list[1].data)
-            assert light_curve.data_frame.sort_index(axis=1).equals(expected_data_frame.sort_index(axis=1))
+            for column_name, fits_index in zip(TessTwoMinuteCadenceColumnName, TessTwoMinuteCadenceMastFitsIndex):
+                assert np.array_equal(light_curve.data_frame[column_name.value], expected_data_frame[fits_index.value])
 
     def test_from_path_factory_light_curve_uses_correct_default_times_and_fluxes(self, fake_hdu_list):
         with patch.object(module.fits, 'open') as mock_open:
             mock_open.return_value.__enter__.return_value = fake_hdu_list
             light_curve = TessTwoMinuteCadenceLightCurve.from_path(Path('TIC 169480782 sector 5.fits'))
-            assert np.array_equal(light_curve.times, fake_hdu_list[1].data[TessTwoMinuteCadenceColumnName.TIME.value])
+            assert np.array_equal(light_curve.times, fake_hdu_list[1].data[TessTwoMinuteCadenceMastFitsIndex.TIME.value])
             assert np.array_equal(light_curve.fluxes,
-                                  fake_hdu_list[1].data[TessTwoMinuteCadenceColumnName.PDCSAP_FLUX.value])
+                                  fake_hdu_list[1].data[TessTwoMinuteCadenceMastFitsIndex.PDCSAP_FLUX.value])
 
     def test_can_get_tic_id_and_sector_from_human_readable_file_name(self):
         tic_id0, sector0 = TessTwoMinuteCadenceLightCurve.get_tic_id_and_sector_from_file_path(
