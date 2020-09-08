b9baa72e0b626465f3b6cfac242f227142c8f69d
Greg Olmschenk <golmschenk@gmail.com>
2020-09-08 15:14:45 -0400

    Adds preloader incrementing and decrementing

--- ramjet/analysis/viewer/preloader.py
+++ ramjet/analysis/viewer/preloader.py
@@ -2,6 +2,7 @@
 Code to load light curves in the background so they show up quickly when displayed.
 """
 import asyncio
+from asyncio import Task
 from collections import deque
 from pathlib import Path
 from typing import Union, List, NamedTuple, Deque
@@ -25,9 +26,10 @@ class Preloader:
 
     def __init__(self):
         self.current_index_light_curve_pair: Union[None, IndexLightCurvePair] = None
-        self.next_index_light_curve_pairs_deque: Deque[IndexLightCurvePair] = deque(maxlen=self.maximum_preloaded)
-        self.previous_index_light_curve_pairs_deque: Deque[IndexLightCurvePair] = deque(maxlen=self.maximum_preloaded)
+        self.next_index_light_curve_pair_deque: Deque[IndexLightCurvePair] = deque(maxlen=self.maximum_preloaded)
+        self.previous_index_light_curve_pair_deque: Deque[IndexLightCurvePair] = deque(maxlen=self.maximum_preloaded)
         self.light_curve_path_list: List[Path] = []
+        self.running_loading_task: Union[Task, None] = None
 
     async def load_surrounding_light_curves(self):
         """
@@ -36,7 +38,7 @@ class Preloader:
         await self.load_next_light_curves()
         await self.load_previous_light_curves()
 
-    async def load_light_curve_at_index_as_current(self, index):
+    async def load_light_curve_at_index_as_current(self, index: int):
         """
         Loads the light curve at the passed index as the current light curve.
 
@@ -52,37 +54,67 @@ class Preloader:
         Preload the next light curves.
         """
         loop = asyncio.get_running_loop()
-        if len(self.next_index_light_curve_pairs_deque) > 0:
-            last_index = self.next_index_light_curve_pairs_deque[-1].index
+        if len(self.next_index_light_curve_pair_deque) > 0:
+            last_index = self.next_index_light_curve_pair_deque[-1].index
         else:
             last_index = self.current_index_light_curve_pair.index
-        while (len(self.next_index_light_curve_pairs_deque) < self.minimum_preloaded and
+        while (len(self.next_index_light_curve_pair_deque) < self.minimum_preloaded and
                last_index != len(self.light_curve_path_list) - 1):
             last_index += 1
             last_light_curve = await loop.run_in_executor(None, self.load_light_curve_from_path,
                                                           self.light_curve_path_list[last_index])
             last_index_light_curve_pair = IndexLightCurvePair(last_index, last_light_curve)
-            self.next_index_light_curve_pairs_deque.append(last_index_light_curve_pair)
+            self.next_index_light_curve_pair_deque.append(last_index_light_curve_pair)
 
     async def load_previous_light_curves(self):
         """
         Preload the previous light curves.
         """
         loop = asyncio.get_running_loop()
-        if len(self.previous_index_light_curve_pairs_deque) > 0:
-            first_index = self.previous_index_light_curve_pairs_deque[0].index
+        if len(self.previous_index_light_curve_pair_deque) > 0:
+            first_index = self.previous_index_light_curve_pair_deque[0].index
         else:
             first_index = self.current_index_light_curve_pair.index
-        while (len(self.previous_index_light_curve_pairs_deque) < self.minimum_preloaded and
+        while (len(self.previous_index_light_curve_pair_deque) < self.minimum_preloaded and
                first_index != 0):
             first_index -= 1
             first_light_curve = await loop.run_in_executor(None, self.load_light_curve_from_path,
                                                            self.light_curve_path_list[first_index])
             first_index_light_curve_pair = IndexLightCurvePair(first_index, first_light_curve)
-            self.previous_index_light_curve_pairs_deque.appendleft(first_index_light_curve_pair)
+            self.previous_index_light_curve_pair_deque.appendleft(first_index_light_curve_pair)
+
+    async def increment(self) -> IndexLightCurvePair:
+        """
+        Increments to the next light curve, and calls loading as necessary.
+
+        :return: The new current index and light curve pair.
+        """
+        self.previous_index_light_curve_pair_deque.append(self.current_index_light_curve_pair)
+        self.current_index_light_curve_pair = self.next_index_light_curve_pair_deque.popleft()
+        await self.refresh_surrounding_light_curve_loading()
+        return self.current_index_light_curve_pair
+
+    async def decrement(self) -> IndexLightCurvePair:
+        """
+        Decrements to the previous light curve, and calls loading as necessary.
+
+        :return: The new current index and light curve pair.
+        """
+        self.next_index_light_curve_pair_deque.appendleft(self.current_index_light_curve_pair)
+        self.current_index_light_curve_pair = self.previous_index_light_curve_pair_deque.pop()
+        await self.refresh_surrounding_light_curve_loading()
+        return self.current_index_light_curve_pair
+
+    async def refresh_surrounding_light_curve_loading(self):
+        """
+        Cancels the existing loading task and starts a new one.
+        """
+        if self.running_loading_task is not None:
+            self.running_loading_task.cancel()
+        asyncio.create_task(self.load_surrounding_light_curves())
 
     @staticmethod
-    def load_light_curve_from_path(path):
+    def load_light_curve_from_path(path: Path) -> LightCurve:
         """
         Loads a light curve from a path.
 
--- tests/analysis/viewer/test_preloader.py
+++ tests/analysis/viewer/test_preloader.py
@@ -1,8 +1,10 @@
+from asyncio import Future
 from collections import deque
-from unittest.mock import Mock
+from unittest.mock import Mock, MagicMock, patch, AsyncMock
 
 import pytest
 
+import ramjet.analysis.viewer.preloader as module
 from ramjet.analysis.viewer.preloader import Preloader, IndexLightCurvePair
 
 
@@ -31,16 +33,16 @@ class TestPreloader:
             return stub_light_curves_dictionary[path]
         mock_load_light_curve_from_path = Mock(side_effect=load_light_curve_side_effect)
         preloader.load_light_curve_from_path = mock_load_light_curve_from_path
-        preloader.next_index_light_curve_pairs_deque = deque(
+        preloader.next_index_light_curve_pair_deque = deque(
             [IndexLightCurvePair(1, stub_light_curves_dictionary['b.fits'])])
 
         await preloader.load_next_light_curves()
 
-        assert len(preloader.next_index_light_curve_pairs_deque) == 3
-        assert preloader.next_index_light_curve_pairs_deque[1].index == 2
-        assert preloader.next_index_light_curve_pairs_deque[1].light_curve == stub_light_curves_dictionary['c.fits']
-        assert preloader.next_index_light_curve_pairs_deque[2].index == 3
-        assert preloader.next_index_light_curve_pairs_deque[2].light_curve == stub_light_curves_dictionary['d.fits']
+        assert len(preloader.next_index_light_curve_pair_deque) == 3
+        assert preloader.next_index_light_curve_pair_deque[1].index == 2
+        assert preloader.next_index_light_curve_pair_deque[1].light_curve == stub_light_curves_dictionary['c.fits']
+        assert preloader.next_index_light_curve_pair_deque[2].index == 3
+        assert preloader.next_index_light_curve_pair_deque[2].light_curve == stub_light_curves_dictionary['d.fits']
 
     @pytest.mark.asyncio
     async def test_loading_next_light_curves_loads_from_current_when_next_deque_is_empty(self):
@@ -55,11 +57,11 @@ class TestPreloader:
 
         await preloader.load_next_light_curves()
 
-        assert len(preloader.next_index_light_curve_pairs_deque) == 2
-        assert preloader.next_index_light_curve_pairs_deque[0].index == 2
-        assert preloader.next_index_light_curve_pairs_deque[0].light_curve == stub_light_curves_dictionary['c.fits']
-        assert preloader.next_index_light_curve_pairs_deque[1].index == 3
-        assert preloader.next_index_light_curve_pairs_deque[1].light_curve == stub_light_curves_dictionary['d.fits']
+        assert len(preloader.next_index_light_curve_pair_deque) == 2
+        assert preloader.next_index_light_curve_pair_deque[0].index == 2
+        assert preloader.next_index_light_curve_pair_deque[0].light_curve == stub_light_curves_dictionary['c.fits']
+        assert preloader.next_index_light_curve_pair_deque[1].index == 3
+        assert preloader.next_index_light_curve_pair_deque[1].light_curve == stub_light_curves_dictionary['d.fits']
 
     @pytest.mark.asyncio
     async def test_loading_previous_light_curves_loads_from_first_in_previous_deque(self):
@@ -70,16 +72,16 @@ class TestPreloader:
             return stub_light_curves_dictionary[path]
         mock_load_light_curve_from_path = Mock(side_effect=load_light_curve_side_effect)
         preloader.load_light_curve_from_path = mock_load_light_curve_from_path
-        preloader.previous_index_light_curve_pairs_deque = deque(
+        preloader.previous_index_light_curve_pair_deque = deque(
             [IndexLightCurvePair(2, stub_light_curves_dictionary['c.fits'])])
 
         await preloader.load_previous_light_curves()
 
-        assert len(preloader.previous_index_light_curve_pairs_deque) == 3
-        assert preloader.previous_index_light_curve_pairs_deque[0].index == 0
-        assert preloader.previous_index_light_curve_pairs_deque[0].light_curve == stub_light_curves_dictionary['a.fits']
-        assert preloader.previous_index_light_curve_pairs_deque[1].index == 1
-        assert preloader.previous_index_light_curve_pairs_deque[1].light_curve == stub_light_curves_dictionary['b.fits']
+        assert len(preloader.previous_index_light_curve_pair_deque) == 3
+        assert preloader.previous_index_light_curve_pair_deque[0].index == 0
+        assert preloader.previous_index_light_curve_pair_deque[0].light_curve == stub_light_curves_dictionary['a.fits']
+        assert preloader.previous_index_light_curve_pair_deque[1].index == 1
+        assert preloader.previous_index_light_curve_pair_deque[1].light_curve == stub_light_curves_dictionary['b.fits']
 
 
     @pytest.mark.asyncio
@@ -91,16 +93,16 @@ class TestPreloader:
             return stub_light_curves_dictionary[path]
         mock_load_light_curve_from_path = Mock(side_effect=load_light_curve_side_effect)
         preloader.load_light_curve_from_path = mock_load_light_curve_from_path
-        preloader.previous_index_light_curve_pairs_deque = deque(
+        preloader.previous_index_light_curve_pair_deque = deque(
             [IndexLightCurvePair(2, stub_light_curves_dictionary['c.fits'])])
 
         await preloader.load_previous_light_curves()
 
-        assert len(preloader.previous_index_light_curve_pairs_deque) == 3
-        assert preloader.previous_index_light_curve_pairs_deque[0].index == 0
-        assert preloader.previous_index_light_curve_pairs_deque[0].light_curve == stub_light_curves_dictionary['a.fits']
-        assert preloader.previous_index_light_curve_pairs_deque[1].index == 1
-        assert preloader.previous_index_light_curve_pairs_deque[1].light_curve == stub_light_curves_dictionary['b.fits']
+        assert len(preloader.previous_index_light_curve_pair_deque) == 3
+        assert preloader.previous_index_light_curve_pair_deque[0].index == 0
+        assert preloader.previous_index_light_curve_pair_deque[0].light_curve == stub_light_curves_dictionary['a.fits']
+        assert preloader.previous_index_light_curve_pair_deque[1].index == 1
+        assert preloader.previous_index_light_curve_pair_deque[1].light_curve == stub_light_curves_dictionary['b.fits']
 
     @pytest.mark.asyncio
     async def test_loading_previous_light_curves_loads_from_current_when_previous_deque_is_empty(self):
@@ -115,8 +117,105 @@ class TestPreloader:
 
         await preloader.load_previous_light_curves()
 
-        assert len(preloader.previous_index_light_curve_pairs_deque) == 2
-        assert preloader.previous_index_light_curve_pairs_deque[0].index == 0
-        assert preloader.previous_index_light_curve_pairs_deque[0].light_curve == stub_light_curves_dictionary['a.fits']
-        assert preloader.previous_index_light_curve_pairs_deque[1].index == 1
-        assert preloader.previous_index_light_curve_pairs_deque[1].light_curve == stub_light_curves_dictionary['b.fits']
+        assert len(preloader.previous_index_light_curve_pair_deque) == 2
+        assert preloader.previous_index_light_curve_pair_deque[0].index == 0
+        assert preloader.previous_index_light_curve_pair_deque[0].light_curve == stub_light_curves_dictionary['a.fits']
+        assert preloader.previous_index_light_curve_pair_deque[1].index == 1
+        assert preloader.previous_index_light_curve_pair_deque[1].light_curve == stub_light_curves_dictionary['b.fits']
+
+    @pytest.mark.asyncio
+    async def test_incrementing_preloader_shifts_light_curve_deques(self):
+        preloader = Preloader()
+        preloader.light_curve_path_list = ['a.fits', 'b.fits', 'c.fits', 'd.fits']
+        stub_light_curves_dictionary = {'a.fits': Mock(), 'b.fits': Mock(), 'c.fits': Mock(), 'd.fits': Mock()}
+        preloader.current_index_light_curve_pair = IndexLightCurvePair(1, stub_light_curves_dictionary['b.fits'])
+        preloader.previous_index_light_curve_pair_deque = deque(
+            [IndexLightCurvePair(0, stub_light_curves_dictionary['a.fits'])])
+        preloader.next_index_light_curve_pair_deque = deque(
+            [IndexLightCurvePair(2, stub_light_curves_dictionary['c.fits'])])
+        preloader.refresh_surrounding_light_curve_loading = AsyncMock()
+
+        new_current = await preloader.increment()
+
+        assert new_current.index == 2
+        assert new_current.light_curve == stub_light_curves_dictionary['c.fits']
+        assert len(preloader.previous_index_light_curve_pair_deque) == 2
+        assert preloader.previous_index_light_curve_pair_deque[-1].index == 1
+        assert len(preloader.next_index_light_curve_pair_deque) == 0
+
+    @pytest.mark.asyncio
+    async def test_decrementing_preloader_shifts_light_curve_deques(self):
+        preloader = Preloader()
+        preloader.light_curve_path_list = ['a.fits', 'b.fits', 'c.fits', 'd.fits']
+        stub_light_curves_dictionary = {'a.fits': Mock(), 'b.fits': Mock(), 'c.fits': Mock(), 'd.fits': Mock()}
+        preloader.current_index_light_curve_pair = IndexLightCurvePair(1, stub_light_curves_dictionary['b.fits'])
+        preloader.previous_index_light_curve_pair_deque = deque(
+            [IndexLightCurvePair(0, stub_light_curves_dictionary['a.fits'])])
+        preloader.next_index_light_curve_pair_deque = deque(
+            [IndexLightCurvePair(2, stub_light_curves_dictionary['c.fits'])])
+        preloader.refresh_surrounding_light_curve_loading = AsyncMock()
+
+        new_current = await preloader.decrement()
+
+        assert new_current.index == 0
+        assert new_current.light_curve is stub_light_curves_dictionary['a.fits']
+        assert len(preloader.next_index_light_curve_pair_deque) == 2
+        assert preloader.next_index_light_curve_pair_deque[0].index == 1
+        assert len(preloader.previous_index_light_curve_pair_deque) == 0
+
+    @pytest.mark.asyncio
+    async def test_incrementing_calls_refresh_of_surrounding_light_curve_loading(self):
+        preloader = Preloader()
+        preloader.next_index_light_curve_pair_deque = Mock()
+        preloader.previous_index_light_curve_pair_deque = Mock()
+        preloader.current_index_light_curve_pair = Mock()
+        mock_refresh_surrounding_light_curve_loading = AsyncMock()
+        preloader.refresh_surrounding_light_curve_loading = mock_refresh_surrounding_light_curve_loading
+
+        _ = await preloader.increment()
+
+        assert mock_refresh_surrounding_light_curve_loading.called
+
+    @pytest.mark.asyncio
+    async def test_decrementing_calls_refresh_of_surrounding_light_curve_loading(self):
+        preloader = Preloader()
+        preloader.next_index_light_curve_pair_deque = Mock()
+        preloader.previous_index_light_curve_pair_deque = Mock()
+        preloader.current_index_light_curve_pair = Mock()
+        mock_refresh_surrounding_light_curve_loading = AsyncMock()
+        preloader.refresh_surrounding_light_curve_loading = mock_refresh_surrounding_light_curve_loading
+
+        _ = await preloader.decrement()
+
+        assert mock_refresh_surrounding_light_curve_loading.called
+
+    @pytest.mark.asyncio
+    async def test_refresh_surrounding_light_curve_loading_cancels_existing_loading_task(self):
+        preloader = Preloader()
+        mock_old_running_loading_task = Mock()
+        preloader.running_loading_task = mock_old_running_loading_task
+
+        await preloader.refresh_surrounding_light_curve_loading()
+
+        assert preloader.running_loading_task.cancel.called
+
+    @pytest.mark.asyncio
+    async def test_refresh_surrounding_light_curve_loading_works_with_no_existing_loading_task(self):
+        preloader = Preloader()
+        preloader.running_loading_task = None
+
+        await preloader.refresh_surrounding_light_curve_loading()
+
+        assert True  # Implicit pass if an error did not occur.
+
+    @pytest.mark.asyncio
+    async def test_refresh_surrounding_light_curve_loading_creates_loading_task(self):
+        preloader = Preloader()
+        stub_coroutine = Mock()
+        mock_load_surrounding_light_curves = Mock(return_value=stub_coroutine)
+        preloader.load_surrounding_light_curves = mock_load_surrounding_light_curves
+
+        with patch.object(module.asyncio, 'create_task') as mock_create_task:
+            _ = await preloader.refresh_surrounding_light_curve_loading()
+
+            assert mock_create_task.call_args[0][0] is stub_coroutine
