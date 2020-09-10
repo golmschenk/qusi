7ee2bf4fc6cee42213dc8d1e36c643faca85beff
Greg Olmschenk <golmschenk@gmail.com>
2020-09-10 16:47:58 -0400

    Clean up docstrings for the CSV path viewer

--- ramjet/analysis/viewer/csv_path_viewer.py
+++ ramjet/analysis/viewer/csv_path_viewer.py
@@ -21,6 +21,9 @@ from ramjet.photometric_database.tess_two_minute_cadence_light_curve import Tess
 
 
 class CsvPathViewer:
+    """
+    A viewer for a CSV file containing a column of paths.
+    """
     def __init__(self):
         self.light_curve_display: Union[LightCurveDisplay, None] = None
         self.preloader: Union[Preloader, None] = None
@@ -31,36 +34,63 @@ class CsvPathViewer:
         self.document: Union[Document, None] = None
 
     async def update_light_curve_with_document_lock(self, light_curve):
+        """
+        Updates the light curve display using the Bokeh document lock.
+
+        :param light_curve: The light curve to update the display with.
+        """
         self.document.add_next_tick_callback(partial(self.light_curve_display.update_from_light_curve,
                                                      lightcurve=light_curve))
 
-    async def display_next_target(self):
+    async def display_next_light_curve(self):
+        """
+        Moves to the next light curve.
+        """
         next_index_light_curve_pair = await self.preloader.increment()
         next_light_curve = next_index_light_curve_pair.light_curve
         await self.update_light_curve_with_document_lock(next_light_curve)
 
-    async def display_previous_target(self):
+    async def display_previous_light_curve(self):
+        """
+        Moves to the previous light curve.
+        """
         previous_index_light_curve_pair = await self.preloader.decrement()
         previous_light_curve = previous_index_light_curve_pair.light_curve
         await self.update_light_curve_with_document_lock(previous_light_curve)
 
-    def create_display_next_target_task(self):
-        asyncio.create_task(self.display_next_target())
+    def create_display_next_light_curve_task(self):
+        """
+        Creates the async task to move to the next light curve.
+        """
+        asyncio.create_task(self.display_next_light_curve())
 
-    def create_display_previous_target_task(self):
-        asyncio.create_task(self.display_previous_target())
+    def create_display_previous_light_curve_task(self):
+        """
+        Creates the async task to move to the previous light curve.
+        """
+        asyncio.create_task(self.display_previous_light_curve())
 
-    def create_target_switching_buttons(self) -> (Button, Button):
+    def create_light_curve_switching_buttons(self) -> (Button, Button):
+        """
+        Creates buttons for switching between light curves.
+        """
         next_button = Button(label='Next target')
-        next_button.on_click(self.create_display_next_target_task)
+        next_button.on_click(self.create_display_next_light_curve_task)
         next_button.sizing_mode = 'stretch_width'
         previous_button = Button(label='Previous target')
-        previous_button.on_click(self.create_display_previous_target_task)
+        previous_button.on_click(self.create_display_previous_light_curve_task)
         previous_button.sizing_mode = 'stretch_width'
         return previous_button, next_button
 
     @classmethod
     def from_csv_path(cls, bokeh_document: Document, csv_path: Path) -> CsvPathViewer:
+        """
+        Creates a viewer from a CSV path containing a light curve path column.
+
+        :param bokeh_document: The Bokeh document to run the viewer in.
+        :param csv_path: The path to the CSV file.
+        :return: The viewer.
+        """
         viewer = cls()
         viewer.document = bokeh_document
         viewer.csv_path = csv_path
@@ -68,7 +98,7 @@ class CsvPathViewer:
                                                                    TessTwoMinuteCadenceLightCurve.flux_column_names,
                                                                    flux_axis_label='Relative flux')
         viewer.light_curve_display.exclude_outliers_from_zoom = True
-        viewer.previous_button, viewer.next_button = viewer.create_target_switching_buttons()
+        viewer.previous_button, viewer.next_button = viewer.create_light_curve_switching_buttons()
         bokeh_document.add_root(viewer.previous_button)
         bokeh_document.add_root(viewer.next_button)
         # bokeh_document.add_root(viewer.information_div)
@@ -76,6 +106,9 @@ class CsvPathViewer:
         return viewer
 
     async def start_preloader(self):
+        """
+        Starts the light curve preloader.
+        """
         paths_data_frame = pd.read_csv(self.csv_path)
         paths = paths_data_frame['Light curve path']
         self.preloader = await Preloader.from_light_curve_path_list(paths)
@@ -84,6 +117,11 @@ class CsvPathViewer:
 
 
 def application(bokeh_document: Document):
+    """
+    The application to run from the Tornado server.
+
+    :param bokeh_document: The Bokeh document to run the viewer in.
+    """
     csv_path = Path('/Users/golmschenk/Code/ramjet/data/viewer_check.csv')
     viewer = CsvPathViewer.from_csv_path(bokeh_document, csv_path)
     loop = asyncio.get_running_loop()
