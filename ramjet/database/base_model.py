ecf7109af0b2e7a2d8d01bdb58f0ed9ca1c47389
Greg Olmschenk <golmschenk@gmail.com>
2020-10-30 18:07:17 -0400

    Adds database code for storing planet transit dispositions

---
+++ ramjet/database/base_model.py
@@ -0,0 +1,31 @@
+"""
+Code for the the base model of the database to build other models from.
+"""
+from typing import Type
+
+from peewee import Model, SqliteDatabase
+
+database = SqliteDatabase('data/database.sqlite3',
+                          pragmas={'journal_mode': 'wal'},
+                          check_same_thread=False)
+
+
+def convert_class_to_table_name(model_class: Type[Model]):
+    """
+    Creates the table name based on the model class.
+
+    :param model_class: The class to create the table name for.
+    :return: The name of the table.
+    """
+    model_name = model_class.__name__
+    return model_name
+
+
+class BaseModel(Model):
+    """
+    A general model for the database tables.
+    """
+    class Meta:
+        """The meta information for the database models."""
+        database = database
+        table_function = convert_class_to_table_name
--- ramjet/database/tess_transiter.py
+++ ramjet/database/tess_transiter.py
@@ -3,19 +3,19 @@ A model for the database entity of a TESS transiter.
 """
 from peewee import AutoField, ForeignKeyField, FloatField, BooleanField
 
-from ramjet.data_interface.metadatabase import MetadatabaseModel
-from ramjet.data_interface.tess_target_metadata_manager import TessTargetMetadata
+from ramjet.database.base_model import BaseModel
+from ramjet.database.tess_target import TessTarget
 
 
-class TessTransiter(MetadatabaseModel):
+class TessTransiter(BaseModel):
     """
     A database model for the database entity of a TESS transiter.
     """
     id = AutoField()
-    target: TessTargetMetadata = ForeignKeyField(TessTargetMetadata)
+    target: TessTarget = ForeignKeyField(TessTarget)
     radius__solar_radii = FloatField(null=True)
     has_known_contamination_ratio = BooleanField(default=True)
     transit_epoch__btjd = FloatField()
     transit_period__days = FloatField()
     transit_duration__days = FloatField()
-    transit_depth = FloatField()
+    transit_relative_depth = FloatField()
