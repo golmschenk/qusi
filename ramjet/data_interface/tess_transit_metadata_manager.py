762243d20c6dc4db19921a252ead482ace35db64
Greg Olmschenk <golmschenk@gmail.com>
2020-11-07 13:41:31 -0500

    Add a way to include local candidates to the training dataset

--- ramjet/data_interface/tess_transit_metadata_manager.py
+++ ramjet/data_interface/tess_transit_metadata_manager.py
@@ -2,6 +2,7 @@
 Code for managing the TESS transit metadata.
 """
 import warnings
+from typing import List
 
 import pandas as pd
 from enum import Enum
@@ -9,6 +10,7 @@ from peewee import IntegerField, CharField
 
 from ramjet.data_interface.metadatabase import MetadatabaseModel, metadatabase
 from ramjet.data_interface.tess_toi_data_interface import TessToiDataInterface, ToiColumns
+from ramjet.database.tess_target import TessTarget
 
 
 class Disposition(Enum):
@@ -73,8 +75,29 @@ class TessTransitMetadataManager:
                 row_count += 1
         print(f'Table built. {row_count} rows added.')
 
+    @staticmethod
+    def add_tic_ids_as_confirmed(tic_ids: List[int]):
+        """
+        Adds the passed TIC IDs as confirmed transits.
+
+        :param tic_ids: The list of TIC IDs.
+        """
+        rows_added = 0
+        with metadatabase.atomic():
+            for tic_id in tic_ids:
+                query = TessTransitMetadata.select().where(TessTransitMetadata.tic_id == tic_id)
+                if query.exists():
+                    continue
+                transit = TessTransitMetadata()
+                transit.tic_id = tic_id
+                transit.disposition = Disposition.CONFIRMED.value
+                transit.save()
+                rows_added += 1
+        print(f'{rows_added} rows added.')
+
 
 if __name__ == '__main__':
     tess_transit_metadata_manager = TessTransitMetadataManager()
     tess_transit_metadata_manager.build_table()
-
+    tess_transit_metadata_manager.add_tic_ids_as_confirmed(
+        TessTarget.get_tic_ids_of_passing_vetted_transiting_planet_candidates())
