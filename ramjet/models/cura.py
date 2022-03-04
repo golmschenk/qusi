8531f9dc52d7ec87310fab4bdc52ef2b5322459f
Greg Olmschenk <greg@olmschenk.com>
2022-03-03 20:30:48 -0500

    Add FFI version of Cura model

--- ramjet/models/cura.py
+++ ramjet/models/cura.py
@@ -214,6 +214,48 @@ class Cura(Model):
         return outputs
 
 
+class FfiCura(Model):
+    """
+    A general convolutional model for light curve data.
+    """
+
+    def __init__(self, number_of_label_values=1, number_of_input_channels: int = 1):
+        super().__init__()
+        self.blocks = []
+        output_channels = 8
+        self.blocks.append(BottleNeckResidualLightCurveNetworkBlock(
+            output_channels=output_channels, input_channels=number_of_input_channels, batch_normalization=False,
+            dropout_rate=0.5))
+        input_channels = output_channels
+        for output_channels in [16, 24, 32, 36, 40, 44, 48, 64, 128]:
+            self.blocks.append(BottleNeckResidualLightCurveNetworkBlock(
+                output_channels=output_channels, input_channels=input_channels, pooling_size=2, dropout_rate=0.5))
+            for _ in range(2):
+                self.blocks.append(BottleNeckResidualLightCurveNetworkBlock(output_channels=output_channels, dropout_rate=0.5))
+            input_channels = output_channels
+        self.final_pooling = AveragePooling1D(pool_size=2)
+        self.prediction_layer = Convolution1D(number_of_label_values, kernel_size=1, activation=sigmoid)
+        self.reshape = Reshape([number_of_label_values])
+
+    def call(self, inputs, training=False, mask=None):
+        """
+        The forward pass of the layer.
+
+        :param inputs: The input tensor.
+        :param training: A boolean specifying if the layer should be in training mode.
+        :param mask: A mask for the input tensor.
+        :return: The output tensor of the layer.
+        """
+        x = inputs
+        for index, block in enumerate(self.blocks):
+            x = block(x, training=training)
+        x = self.final_pooling(x, training=training)
+        x = self.prediction_layer(x, training=training)
+        outputs = self.reshape(x, training=training)
+        return outputs
+
+
+
 class CuraNoSigmoid(Model):
     """
     A general convolutional model for light curve data.
