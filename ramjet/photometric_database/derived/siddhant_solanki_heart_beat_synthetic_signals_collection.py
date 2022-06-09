5cda9317d8f458e81422ad77bc8877f041348fa8
Greg Olmschenk <greg@olmschenk.com>
2022-06-09 13:52:15 -0400

    Add the hard negative dataset to Siddhant Solanki's heart beat dataset

--- ramjet/photometric_database/derived/siddhant_solanki_heart_beat_synthetic_signals_collection.py
+++ ramjet/photometric_database/derived/siddhant_solanki_heart_beat_synthetic_signals_collection.py
@@ -1,12 +1,17 @@
 import re
 
+from peewee import Select
+
+from ramjet.data_interface.tess_ffi_light_curve_metadata_manager import TessFfiLightCurveMetadata
+from ramjet.photometric_database.derived.tess_ffi_light_curve_collection import TessFfiLightCurveCollection
+
 try:
     # be ready for 3.10 when it drops
     from enum import StrEnum
 except ImportError:
     from backports.strenum import StrEnum
 from pathlib import Path
-from typing import Iterable
+from typing import Iterable, Union, List
 
 import numpy as np
 import pandas as pd
@@ -32,11 +37,13 @@ class SiddhantSolankiHeartBeatSyntheticSignalsCollection(LightCurveCollection):
         return heart_beat_synthetic_signals
 
     def load_times_and_magnifications_from_path(self, path: Path) -> (np.ndarray, np.ndarray):
-        synthetic_signal_data_frame = pd.read_csv(path, names=[ColumnName.TIME__DAYS, ColumnName.MAGNIFICATION],
+        synthetic_signal_data_frame = pd.read_csv(path, names=[ColumnName.MAGNIFICATION],
                                                   skipinitialspace=True, delim_whitespace=True, skiprows=1)
         synthetic_signal_data_frame.dropna(inplace=True)
-        times = synthetic_signal_data_frame[ColumnName.TIME__DAYS].values
         magnifications = synthetic_signal_data_frame[ColumnName.MAGNIFICATION].values
+        step_size__days = 0.0069444444
+        times = np.arange(0, magnifications.shape[0] * step_size__days, step_size__days)
+        assert times.shape[0] == magnifications.shape[0]
         return times, magnifications
 
 
@@ -53,9 +60,30 @@ class SiddhantSolankiNonHeartBeatSyntheticSignalsCollection(LightCurveCollection
         return non_heart_beat_synthetic_signals
 
     def load_times_and_magnifications_from_path(self, path: Path) -> (np.ndarray, np.ndarray):
-        synthetic_signal_data_frame = pd.read_csv(path, names=[ColumnName.TIME__DAYS, ColumnName.MAGNIFICATION],
-                                                  skipinitialspace=True, delim_whitespace=True)
+        synthetic_signal_data_frame = pd.read_csv(path, names=[ColumnName.MAGNIFICATION],
+                                                  skipinitialspace=True, delim_whitespace=True, skiprows=1)
         synthetic_signal_data_frame.dropna(inplace=True)
-        times = synthetic_signal_data_frame[ColumnName.TIME__DAYS].values
         magnifications = synthetic_signal_data_frame[ColumnName.MAGNIFICATION].values
+        step_size__days = 0.0069444444
+        times = np.arange(0, magnifications.shape[0] * step_size__days, step_size__days)
+        assert times.shape[0] == magnifications.shape[0]
         return times, magnifications
+
+class TessFfiHeartBeatHardNegativeLightcurveCollection(TessFfiLightCurveCollection):
+    """
+    A class representing the collection of TESS two minute cadence lightcurves containing eclipsing binaries.
+    """
+    def __init__(self, dataset_splits: Union[List[int], None] = None,
+                 magnitude_range: (Union[float, None], Union[float, None]) = (None, None)):
+        super().__init__(dataset_splits=dataset_splits, magnitude_range=magnitude_range)
+        self.label = 0
+        self.hard_negative_ids = list(pd.read_csv('data/heart_beat_hard_negatives.csv')['tic_id'].values)
+
+    def get_sql_query(self) -> Select:
+        """
+        Gets the SQL query for the database models for the lightcurve collection.
+        :return: The SQL query.
+        """
+        query = super().get_sql_query()
+        query = query.where(TessFfiLightCurveMetadata.tic_id.in_(self.hard_negative_ids))
+        return query
