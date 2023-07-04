fe844fbb1326fd12139d670a2054627a71aa7625
golmschenk <greg@olmschenk.com>
2023-07-03 23:00:23 -0500

    Make a light curve collection class that does not include label

--- qusi/light_curve_collection.py
+++ qusi/light_curve_collection.py
@@ -19,6 +19,44 @@ class LightCurveCollection:
             load_times_and_fluxes_from_path_function
         self.random = random.Random(0)
 
+    @classmethod
+    def new(cls,
+            get_paths_function: Callable[[], Iterable[Path]],
+            load_times_and_fluxes_from_path_function: Callable[[Path], LightCurve],
+            load_label_from_path_function: Callable[[Path], int]
+            ) -> Self:
+        """
+        Creates a new light curve collection.
+
+        :param get_paths_function: The function to load the list of paths.
+        :param load_times_and_fluxes_from_path_function: The function to load the times and fluxes from the light curve.
+        :return: The light curve collection.
+        """
+        return cls(get_paths_function=get_paths_function,
+                   load_times_and_fluxes_from_path_function=load_times_and_fluxes_from_path_function)
+
+    def get_shuffled_paths(self) -> Iterable[Path]:
+        """
+        Gets the shuffled paths iterable.
+
+        :return: The shuffled paths iterable.
+        """
+        light_curve_paths = list(self.get_paths_function())
+        self.random.shuffle(light_curve_paths)
+        return light_curve_paths
+
+    def light_curve_iter(self) -> Iterator[LightCurve]:
+        """
+        Get the iterable that will iterate through the light curves of the collection.
+
+        :return: The iterable of the light curves.
+        """
+        light_curve_paths = self.get_shuffled_paths()
+        for light_curve_path in light_curve_paths:
+            times, fluxes = self.load_times_and_fluxes_from_path_function(light_curve_path)
+            light_curve = LightCurve.new(times, fluxes)
+            yield light_curve
+
 
 class LabeledLightCurveCollection(LightCurveCollection):
     """
@@ -83,16 +121,6 @@ class LabeledLightCurveCollection(LightCurveCollection):
             light_curve = LightCurve.new(times, fluxes)
             light_curve_observation = LightCurveObservation.new(light_curve, label)
             yield light_curve_observation
-
-    def get_shuffled_paths(self) -> Iterable[Path]:
-        """
-        Gets the shuffled paths iterable.
-
-        :return: The shuffled paths iterable.
-        """
-        light_curve_paths = list(self.get_paths_function())
-        self.random.shuffle(light_curve_paths)
-        return light_curve_paths
 
 
 def create_constant_label_for_path_function(label: int) -> Callable[[Path], int]:
