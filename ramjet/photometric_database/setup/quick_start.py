62c7f24286a7a549859a4cb273d09b78b4ae71a7
golmschenk <greg@olmschenk.com>
2023-07-21 19:19:59 -0400

    Make the download portion of the quick_start script available on its own

--- ramjet/photometric_database/setup/quick_start.py
+++ ramjet/photometric_database/setup/quick_start.py
@@ -8,18 +8,22 @@ from ramjet.data_interface.tess_toi_data_interface import TessToiDataInterface
 from ramjet.photometric_database.setup.tess_two_minute_cadence_transit_metadata import build_tables
 
 
-def download():
+def download_and_build_tables():
     """
     Downloads and prepares the data needed for the quick start tutorial.
     """
+    download()
+    build_tables()
+
+
+def download():
     tess_data_interface = TessDataInterface()
     tess_data_interface.download_two_minute_cadence_light_curves(Path('data/tess_two_minute_cadence_light_curves'),
                                                                  limit=10000)
     tess_toi_data_interface = TessToiDataInterface()
     tess_toi_data_interface.download_exofop_toi_light_curves_to_directory(
         Path('data/tess_two_minute_cadence_light_curves'))
-    build_tables()
 
 
 if __name__ == '__main__':
-    download()
+    download_and_build_tables()
