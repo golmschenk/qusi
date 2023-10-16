92f8a4124474532cbbaefc478391a12200907ef2
golmschenk <greg@olmschenk.com>
2023-10-16 16:11:09 -0400

    Add transit train script

---
+++ scripts/transit_train.py
@@ -0,0 +1,71 @@
+from pathlib import Path
+
+import numpy as np
+
+from qusi.hadryss_model import Hadryss
+from qusi.light_curve_collection import LabeledLightCurveCollection, \
+    create_constant_label_for_path_function
+from qusi.light_curve_dataset import LightCurveDataset
+from qusi.single_dense_layer_model import SingleDenseLayerBinaryClassificationModel
+from qusi.train_session import TrainSession
+from ramjet.photometric_database.tess_two_minute_cadence_light_curve import TessMissionLightCurve
+
+
+def main():
+    def get_negative_train_paths():
+        return list(Path('data/spoc_transit_experiment/train/negatives').glob('*.fits'))
+
+
+    def get_negative_validation_paths():
+        return list(Path('data/spoc_transit_experiment/validation/negatives').glob('*.fits'))
+
+
+    def get_positive_train_paths():
+        return list(Path('data/spoc_transit_experiment/train/positives').glob('*.fits'))
+
+
+    def get_positive_validation_paths():
+        return list(Path('data/spoc_transit_experiment/validation/positives').glob('*.fits'))
+
+
+    def load_times_and_fluxes_from_path(path: Path) -> (np.ndarray, np.ndarray):
+        light_curve = TessMissionLightCurve.from_path(path)
+        return light_curve.times, light_curve.fluxes
+
+
+    positive_label_function = create_constant_label_for_path_function(1)
+    negative_label_function = create_constant_label_for_path_function(0)
+
+    positive_train_light_curve_collection = LabeledLightCurveCollection.new(
+        get_paths_function=get_positive_train_paths,
+        load_times_and_fluxes_from_path_function=load_times_and_fluxes_from_path,
+        load_label_from_path_function=positive_label_function)
+    negative_train_light_curve_collection = LabeledLightCurveCollection.new(
+        get_paths_function=get_negative_train_paths,
+        load_times_and_fluxes_from_path_function=load_times_and_fluxes_from_path,
+        load_label_from_path_function=negative_label_function)
+    positive_validation_light_curve_collection = LabeledLightCurveCollection.new(
+        get_paths_function=get_positive_validation_paths,
+        load_times_and_fluxes_from_path_function=load_times_and_fluxes_from_path,
+        load_label_from_path_function=positive_label_function)
+    negative_validation_light_curve_collection = LabeledLightCurveCollection.new(
+        get_paths_function=get_negative_validation_paths,
+        load_times_and_fluxes_from_path_function=load_times_and_fluxes_from_path,
+        load_label_from_path_function=negative_label_function)
+
+    train_light_curve_dataset = LightCurveDataset.new(
+        standard_light_curve_collections=[positive_train_light_curve_collection,
+                                          negative_train_light_curve_collection])
+    validation_light_curve_dataset = LightCurveDataset.new(
+        standard_light_curve_collections=[positive_validation_light_curve_collection,
+                                          negative_validation_light_curve_collection])
+    model = Hadryss()
+    train_session = TrainSession.new(train_datasets=[train_light_curve_dataset],
+                                     validation_datasets=[validation_light_curve_dataset],
+                                     model=model, batch_size=100, cycles=30, train_steps_per_cycle=5000,
+                                     validation_steps_per_cycle=500)
+    train_session.run()
+
+
+if __name__ == '__main__':
+    main()
