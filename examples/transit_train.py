007a85e4f35bbfd5959d407e8c3e015cb758ae6c
golmschenk <greg@olmschenk.com>
2024-03-09 17:44:46 -0500

    Make the default number of cycles in the training example to be 20 as users typically let it run fully

--- examples/transit_train.py
+++ examples/transit_train.py
@@ -9,7 +9,7 @@ def main():
     validation_light_curve_dataset = get_transit_validation_dataset()
     model = Hadryss.new()
     train_hyperparameter_configuration = TrainHyperparameterConfiguration.new(
-        batch_size=100, cycles=100, train_steps_per_cycle=100, validation_steps_per_cycle=10)
+        batch_size=100, cycles=20, train_steps_per_cycle=100, validation_steps_per_cycle=10)
     train_session(train_datasets=[train_light_curve_dataset], validation_datasets=[validation_light_curve_dataset],
                   model=model, hyperparameter_configuration=train_hyperparameter_configuration)
 
