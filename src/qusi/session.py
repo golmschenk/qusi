4dc9e1f6baef83fc2e2e427a9c2cef27d401612a
golmschenk <greg@olmschenk.com>
2024-05-06 18:17:53 -0400

    Add the infinite_datasets_test_session

--- src/qusi/session.py
+++ src/qusi/session.py
@@ -4,6 +4,7 @@ Session related public interface.
 from qusi.internal.device import get_device
 from qusi.internal.finite_test_session import finite_datasets_test_session
 from qusi.internal.infer_session import infer_session
+from qusi.internal.infinite_datasets_test_session import infinite_datasets_test_session
 from qusi.internal.train_hyperparameter_configuration import TrainHyperparameterConfiguration
 from qusi.internal.train_logging_configuration import TrainLoggingConfiguration
 from qusi.internal.train_system_configuration import TrainSystemConfiguration
@@ -13,6 +14,7 @@ __all__ = [
     'finite_datasets_test_session',
     'get_device',
     'infer_session',
+    'infinite_datasets_test_session',
     'TrainHyperparameterConfiguration',
     'TrainLoggingConfiguration',
     'TrainSystemConfiguration',
