250f2a648bb405d1e24e74e60024d9cc0f6e6875
golmschenk <greg@olmschenk.com>
2024-05-15 14:21:10 -0400

    Move transforms to transforms file

--- src/qusi/internal/light_curve_transforms.py
+++ src/qusi/internal/light_curve_transforms.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 import numpy as np
 import numpy.typing as npt
 import torch
@@ -37,4 +39,48 @@ def pair_array_to_tensor(
 def randomly_roll_elements(example: np.ndarray) -> np.ndarray:
     """Randomly rolls the elements."""
     example = np.roll(example, np.random.randint(example.shape[0]), axis=0)
+    return example
+
+
+def normalize_tensor_by_modified_z_score(tensor: Tensor) -> Tensor:
+    """
+    Normalizes a tensor by a modified z-score. That is, normalizes the values of the tensor based on the median
+    absolute deviation.
+
+    :param tensor: The tensor to normalize.
+    :return: The normalized tensor.
+    """
+    median = torch.median(tensor)
+    deviation_from_median = tensor - median
+    absolute_deviation_from_median = torch.abs(deviation_from_median)
+    median_absolute_deviation_from_median = torch.median(absolute_deviation_from_median)
+    if median_absolute_deviation_from_median != 0:
+        modified_z_score = (
+                0.6745 * deviation_from_median / median_absolute_deviation_from_median
+        )
+    else:
+        modified_z_score = torch.zeros_like(tensor)
+    return modified_z_score
+
+
+def make_uniform_length(
+        example: np.ndarray, length: int, *, randomize: bool = True
+) -> np.ndarray:
+    """Makes the example a specific length, by clipping those too large and repeating those too small."""
+    if len(example.shape) not in [1, 2]:  # Only tested for 1D and 2D cases.
+        raise ValueError(
+            f"Light curve dimensions expected to be in [1, 2], but found {len(example.shape)}"
+        )
+    if randomize:
+        example = randomly_roll_elements(example)
+    if example.shape[0] == length:
+        pass
+    elif example.shape[0] > length:
+        example = example[:length]
+    else:
+        elements_to_repeat = length - example.shape[0]
+        if len(example.shape) == 1:
+            example = np.pad(example, (0, elements_to_repeat), mode="wrap")
+        else:
+            example = np.pad(example, ((0, elements_to_repeat), (0, 0)), mode="wrap")
     return example
