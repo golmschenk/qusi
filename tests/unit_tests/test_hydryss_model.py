96ff8ba1623af9278bf05da75eeb7f781a01682e
golmschenk <greg@olmschenk.com>
2024-08-06 13:22:58 -0400

    Correct tests

--- tests/unit_tests/test_hydryss_model.py
+++ tests/unit_tests/test_hydryss_model.py
@@ -1,7 +1,7 @@
 import torch
 
 from qusi.internal.hadryss_model import Hadryss, HadryssBinaryClassEndModule, \
-    HadryssMultiClassEndModule
+    HadryssMultiClassScoreEndModule
 
 
 def test_lengths_give_correct_output_size():
@@ -41,7 +41,7 @@ def test_binary_classification_end_module_produces_expected_shape():
 
 
 def test_multi_class_classification_end_module_produces_expected_shape():
-    model = Hadryss.new(input_length=100, end_module=HadryssMultiClassEndModule.new(number_of_classes=3))
+    model = Hadryss.new(input_length=100, end_module=HadryssMultiClassScoreEndModule.new(number_of_classes=3))
 
     output = model(torch.arange(7 * 100, dtype=torch.float32).reshape([7, 100]))
 
