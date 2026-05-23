4f660b59ebcc76b72182a04021df9ec9b61a5e04
golmschenk <greg@olmschenk.com>
2026-05-23 13:55:00 -0400

    Add distributed logging wrapper

--- src/qusi/internal/distributed.py
+++ src/qusi/internal/distributed.py
@@ -1,13 +1,105 @@
+import functools
+import logging
 import os
+import sys
+from contextlib import contextmanager
+from pathlib import Path
+from typing import Generator, TextIO, Callable, Any, ParamSpec, TypeVar
+
+logger = logging.getLogger(__name__)
+
+P = ParamSpec('P')
+R = TypeVar('R')
 
 
-def ensure_torchrun_environment_variables():
+def ensure_torchrun_environment_variables() -> None:
+    """
+    Add fake torchrun environment variables if they are not already active.
+    """
     if 'RANK' not in os.environ:
         # The script was not called with `torchrun` and environment variables need to be set manually.
         os.environ['RANK'] = str(0)
+        os.environ['GROUP_RANK'] = str(0)
         os.environ['LOCAL_RANK'] = str(0)
         os.environ['WORLD_SIZE'] = str(1)
         os.environ['LOCAL_WORLD_SIZE'] = str(1)
         os.environ['MASTER_ADDR'] = 'localhost'
         os.environ['MASTER_PORT'] = '35728'
         os.environ['TORCHELASTIC_RESTART_COUNT'] = str(0)
+
+
+def distributed_logging(decorated_function: Callable[P, R]) -> Callable[P, R]:
+    """
+    Enables distributed logging. Each torch worker will log to a separate file. Library extensions also log to this
+    file.
+
+    :param decorated_function: The function that should be logged in a distributed way.
+    :return:
+    """
+    if 'RANK' not in os.environ:
+        return decorated_function
+    if 'QUSI_SESSION_DIRECTORY' not in os.environ:
+        logging.warning('Distributed logging session directory environment variable `QUSI_SESSION_DIRECTORY` is not ' +
+                        'set. Logs from all ranks will be displayed in stdout.')
+        return decorated_function
+    session_directory = Path(os.environ['QUSI_SESSION_DIRECTORY'])
+    session_directory.mkdir(parents=True, exist_ok=True)
+    rank_logging_path = session_directory.joinpath(
+        f'rank_{os.environ["RANK"]}_group_rank_{os.environ["GROUP_RANK"]}_local_rank_{os.environ["GROUP_RANK"]}.log')
+
+    @functools.wraps(decorated_function)
+    def redirect_wrapper(*args: P.args, **kwargs: P.kwargs) -> R:
+        """
+        The wrapper that applies the redirect.
+
+        :param args: The args passed through to the decorated function.
+        :param kwargs: The kwargs passed through to the decorated function.
+        :return: The wrapper function.
+        """
+        with redirected_output(rank_logging_path):
+            return decorated_function(*args, **kwargs)
+
+    return redirect_wrapper
+
+
+@contextmanager
+def redirected_output(path: Path) -> Generator[None, None, None]:
+    """
+    Redirect stdout and stderr to a path. Applies to extension libraries as well.
+
+    :param path: The path to redirect to.
+    :return: The redirect context manager.
+    """
+    stdout_file_descriptor = sys.stdout.fileno()
+    stderr_file_descriptor = sys.stderr.fileno()
+
+    def redirect_stdout(file_handle_: TextIO) -> None:
+        """
+        Redirect stdout to a file handle.
+
+        :param file_handle_: The file handle.
+        """
+        sys.stdout.close()
+        os.dup2(file_handle_.fileno(), stdout_file_descriptor)
+        sys.stdout = os.fdopen(stdout_file_descriptor, 'a')
+
+    def redirect_stderr(file_handle_: TextIO) -> None:
+        """
+        Redirect stderr to a file handle.
+
+        :param file_handle_: The file handle.
+        """
+        sys.stderr.close()
+        os.dup2(file_handle_.fileno(), stderr_file_descriptor)
+        sys.stderr = os.fdopen(stderr_file_descriptor, 'a')
+
+    with (os.fdopen(os.dup(stdout_file_descriptor), 'a') as original_stdout,
+          os.fdopen(os.dup(stderr_file_descriptor), 'a') as original_stderr):
+        with open(path, 'a') as file:
+            redirect_stdout(file_handle_=file)
+            redirect_stderr(file_handle_=file)
+        try:
+            yield
+        finally:
+            redirect_stdout(file_handle_=original_stdout)
+            redirect_stderr(file_handle_=original_stderr)
